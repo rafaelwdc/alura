b1220065b22097810baded11270977bec5d383a0
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-07-16 22:20:17 -0300

    projeto typescript

--- Alura/estudos/typescript/typescript-curso-1-arquivos-iniciais/app/app.ts
+++ Alura/estudos/typescript/typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -1,4 +1,5 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js';
+
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
 form.addEventListener('submit', event => {
--- Alura/estudos/typescript/typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
+++ Alura/estudos/typescript/typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
@@ -1,27 +1,14 @@
 export class Negociacao {
-    #data;
-    #quantidade;
-    #valor;
-    
-    constructor(data, quantidade, valor){
-        this.#data = data;
-        this.#quantidade = quantidade;
-        this.#valor = valor;
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
-
-    get data(){
-        return this.#data;
+    get volume() {
+        return this.quantidade * this.valor;
     }
-
-    get quantidade(){
-        return this.#quantidade;
-    }
-
-    get valor(){
-        return this.#valor;
-    }
-
-    get volume (){
-        return this.#quantidade * this.#valor
+    get data() {
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
