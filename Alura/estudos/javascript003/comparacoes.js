653ad5d164667f814052e9619532dd025016539b
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-07-27 22:53:11 -0300

    vida inteligente na madrugada

---
+++ Alura/estudos/javascript003/comparacoes.js
@@ -0,0 +1,17 @@
+// Comparação implicita
+// ==  - compara apenas o valor
+// ===  - compara o valor e o tipo
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+// Typeof
+console.log(typeof numero);
+console.log(typeof texto);
+
+// Comparação explicita
+
+Number();
+String();
--- Alura/estudos/javascript003/conversoes.js
+++ Alura/estudos/javascript003/conversoes.js
@@ -3,8 +3,10 @@
 
 // Conversão implícita
  const numero = 456;
- const numeroString = Number("456a");
+ const numeroString = Number("456");
 
+ // Number();
+ // String();
  console.log(numero + numeroString);
 
  // Conversão explícita
