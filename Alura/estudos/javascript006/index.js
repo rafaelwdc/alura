f8c2f6bc032b99064fbbd8f85cc84578a14ed879
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-07-31 06:02:54 -0300

    vida inteligente na madrugada

---
+++ Alura/estudos/javascript006/index.js
@@ -0,0 +1,18 @@
+const livros = require('./listaLivros');
+const menorValor = require('./menorValor');
+
+for (let atual = 0; atual < livros.length; atual++) {
+  let menor = menorValor(livros, atual);
+
+  let livroAtual = livros[atual];
+  console.log(livros[atual]);
+  let livroMenorPreco = livros[menor];
+  console.log(livros[menor]);
+
+
+  livros[atual] = livroMenorPreco;
+  livros[menor] = livroAtual;
+
+};
+
+console.log(livros);
--- Alura/testes/teste1/main2.js
+++ Alura/testes/teste1/main2.js
@@ -14,22 +14,22 @@ class Conta extends Clientes{
         this.saldo = saldo;
     };
     exibirSaldo(){
-        console.log(`--  Olá, Sr. ${this.nome}. \n-- Saldo disponivel: R$ ${this.saldo}\n`);
+        console.log(`--  Olá, Sr(a) ${this.nome}. \n-- Saldo disponivel: R$ ${this.saldo}\n`);
     }
 
     depositar(valor) {
         this.saldo += valor; 
-        console.log(`--  Olá, Sr. ${this.nome}. \n-- Deposito de R$ ${valor} efetuado com Sucesso! Seu saldo agora é de R$ ${this.saldo}\n`);
+        console.log(`--  Olá, Sr(a) ${this.nome}. \n-- Deposito de R$ ${valor} efetuado com Sucesso! Seu saldo agora é de R$ ${this.saldo}\n`);
     };
     sacar(valor) {
         this.saldo -= valor; 
-        console.log(`--  Olá, Sr. ${this.nome}. \n-- Saque de R$ ${valor} efetuado com Sucesso! Seu saldo agora é de R$ ${this.saldo}\n`);
+        console.log(`--  Olá, Sr(a) ${this.nome}. \n-- Saque de R$ ${valor} efetuado com Sucesso! Seu saldo agora é de R$ ${this.saldo}\n`);
     };
     transferir(valor, cliente1, cliente2) {
         cliente1.saldo -= valor;
-        console.log(`--  Olá, Sr(a). ${cliente1.nome}. \n-- Trasferência para o(a) Sr(a) ${cliente2.nome} no valor de R$ ${valor} efetuada com sucesso!\n-- Saldo disponivel: R$ ${cliente1.saldo}\n`);
+        console.log(`--  Olá, Sr(a) ${cliente1.nome}. \n-- Trasferência para o(a) Sr(a) ${cliente2.nome} no valor de R$ ${valor} efetuada com sucesso!\n-- Saldo disponivel: R$ ${cliente1.saldo}\n`);
         cliente2.saldo += valor;
-        console.log(`--  Olá, Sr.(a) ${cliente2.nome}. \n-- Trasferência para o(a) Sr(a) ${cliente1.nome} no valor de R$ ${valor} efetuada com sucesso!\n-- Saldo disponivel: R$ ${cliente2.saldo}\n`);
+        console.log(`--  Olá, Sr(a) ${cliente2.nome}. \n-- Trasferência para o(a) Sr(a) ${cliente1.nome} no valor de R$ ${valor} efetuada com sucesso!\n-- Saldo disponivel: R$ ${cliente2.saldo}\n`);
 
     };
 };
@@ -39,20 +39,19 @@ console.log(`\n\n//----------------------- # [ Começo ] # ---------------------
 
 const contaRafael = new Conta("Rafael",29,"81510030","77742","0",1000);
 
-console.log(`Sr ${contaRafael.nome}, Conta criada com Sucesso.\n-- Nome: ${contaRafael.nome};\n-- Idade: ${contaRafael.idade} anos de idade;\n-- Agência: ${contaRafael.agencia};\n-- Conta  ${contaRafael.conta}-${contaRafael.digito};\n-- Saldo disponivel: R$ ${contaRafael.saldo}. \n`);
+console.log(`Sr(a) ${contaRafael.nome}, Conta criada com Sucesso.\n-- Nome: ${contaRafael.nome};\n-- Idade: ${contaRafael.idade} anos de idade;\n-- Agência: ${contaRafael.agencia};\n-- Conta  ${contaRafael.conta}-${contaRafael.digito};\n-- Saldo disponivel: R$ ${contaRafael.saldo}. \n`);
 
 //console.log(contaRafael);
 
 const contaPatricky = new Conta("Patricky",31,"81910180","77742","5",1000);
 
-console.log(`Sr ${contaPatricky.nome}, Conta criada com Sucesso.\n-- Nome: ${contaPatricky.nome};\n-- Idade: ${contaPatricky.idade} de idade;\n-- Agência: ${contaPatricky.agencia};\n-- Conta  ${contaPatricky.conta}-${contaPatricky.digito};\n-- Saldo disponivel: R$ ${contaPatricky.saldo}. \n`);
+console.log(`Sr(a) ${contaPatricky.nome}, Conta criada com Sucesso.\n-- Nome: ${contaPatricky.nome};\n-- Idade: ${contaPatricky.idade} de idade;\n-- Agência: ${contaPatricky.agencia};\n-- Conta  ${contaPatricky.conta}-${contaPatricky.digito};\n-- Saldo disponivel: R$ ${contaPatricky.saldo}. \n`);
 
 //console.log(contaPatricky);
 
 contaPatricky.exibirSaldo();
 contaRafael.exibirSaldo();
 
-;
 contaPatricky.depositar(1000);
 contaRafael.depositar(1000);
 contaPatricky.exibirSaldo();
