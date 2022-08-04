9c440279500abe2a3ada362473b1cc1f1a7ecad2
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-08-04 05:41:31 -0300

    vida inteligente na madrugada

---
+++ Alura/estudos/node.js/index.js
@@ -0,0 +1,69 @@
+const chalk = require('chalk');
+const fs = require('fs');
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro));
+};
+
+async function pegaArquivo(caminhoDoArquivo) {
+    const encoding = "UTF-8";
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.green(texto));
+    } catch {
+        trataErro(erro);
+    };
+};
+
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'UTF-8';
+//     fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => chalk.green(console.log(texto)))
+//     .catch((erro) => trataErro(erro));
+// };
+
+
+
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'UTF-8'
+//     fs.readFile(caminhoDoArquivo,encoding, (erro,texto) =>{
+//         if (erro) {
+//             trataErro(erro);
+//         };
+//         console.log(chalk.green(texto));
+//     });
+// };
+
+pegaArquivo('./arquivos/texto1.md');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(chalk.blue("Vamos começar"));
+
+// const paragrafo = 'Texto retornado por uma função;';
+// function texto(string) {
+//     return string;
+// };
+// console.log(texto(paragrafo));
