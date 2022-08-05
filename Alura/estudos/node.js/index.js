a74cca182601c168aa9ca2597166b62668247992
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-08-05 06:29:59 -0300

    vida inteligente na madrugada

--- Alura/estudos/node.js/index.js
+++ Alura/estudos/node.js/index.js
@@ -1,6 +1,21 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+const texto = 'A interface File provê informações sobre arquivos e permite ao JavaScript a acessar seu conteúdo.São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégios podem criar objetos File representando qualquer arquivo local sem a interação do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/Files#implementation_notes) para mais informações.)'
+
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s]*.[^\s]*)\)/gm;
+    const arrayResultado = [];
+    let temp;
+    while((temp = regex.exec(texto)) != null) {
+        arrayResultado.push({ [temp[1]]: [temp[2]] });
+    };
+
+    return arrayResultado;
+};
+
+extraiLinks(texto);
+
 function trataErro(erro) {
     throw new Error(chalk.red(erro));
 };
@@ -15,27 +30,23 @@ async function pegaArquivo(caminhoDoArquivo) {
     };
 };
 
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'UTF-8';
+    fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => chalk.green(console.log(texto)))
+    .catch((erro) => trataErro(erro));
+};
 
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'UTF-8';
-//     fs.promises
-//     .readFile(caminhoDoArquivo, encoding)
-//     .then((texto) => chalk.green(console.log(texto)))
-//     .catch((erro) => trataErro(erro));
-// };
-
-
-
-
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'UTF-8'
-//     fs.readFile(caminhoDoArquivo,encoding, (erro,texto) =>{
-//         if (erro) {
-//             trataErro(erro);
-//         };
-//         console.log(chalk.green(texto));
-//     });
-// };
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'UTF-8'
+    fs.readFile(caminhoDoArquivo,encoding, (erro,texto) =>{
+        if (erro) {
+            trataErro(erro);
+        };
+        console.log(chalk.green(texto));
+    });
+};
 
 pegaArquivo('./arquivos/texto1.md');
 
