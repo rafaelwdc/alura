e6c053e4d855ddacc71406caf876c591d2fe258a
Rafael Matheus de Freitas <106930852+rafaelwdc@users.noreply.github.com>
2022-08-07 08:31:05 -0300

    vida inteligente ontem?

--- Alura/testes/teste3/script.js
+++ Alura/testes/teste3/script.js
@@ -1,5 +1,6 @@
+const chalk = require('chalk')
 const tabuada = 777;
 for (let i = 0; i <= 777;i++) {
 let conta = i * tabuada;
-console.log(`${i} x ${tabuada} = ${conta}`);
+console.log(chalk.blue(`${i} x ${tabuada} = ${conta}`));
 };
