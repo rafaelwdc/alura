086f2514399fee6d745caed334afce5b2f73ebb6
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-08-12 06:00:20 -0300

    vida inteligente na madrugada

--- Alura/estudos/javascript-browser/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Alura/estudos/javascript-browser/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -8,46 +8,50 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-
-        let data = new Date(...
-            this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
-
+        
         let negociacao = new Negociacao(
-            data,
+            DateHelper.textoParaDate(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
-        );
+            );
 
-
-        console.log(negociacao);
+            console.log(negociacao);
+            console.log(DateHelper.dataParaTexto(negociacao.data));
+            
+        };
     };
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let negociacao = new Negociacao(
-//     this._inputData.value,
-//     this._inputQuantidade.value,
-//     this._inputValor.value
-// );
-
-// console.log(this._inputQuantidade.value);
-// console.log(this._inputValor.value);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // let diaMesAno = negociacao.data.getDate() 
+    // + '/' + (negociacao.data.getMonth() + 1)
+    // + '/' + negociacao.data.getFullYear();
+    
+    // let data = new Date(...
+    //     this._inputData.value
+    //         .split('-')
+    //         .map((item, indice) => item - indice % 2)
+    // );
+    
+    // let negociacao = new Negociacao(
+        //     this._inputData.value,
+        //     this._inputQuantidade.value,
+        //     this._inputValor.value
+        // );
+        
+        // console.log(this._inputQuantidade.value);
+        // console.log(this._inputValor.value);
---
+++ Alura/estudos/javascript-browser/aluraframe/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,21 @@
+class DateHelper {
+
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada');
+    };
+    
+    static dataParaTexto(data) {
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`
+    };
+
+    static textoParaDate(texto) {
+
+        if(/\d{4}-\d{2}-\d{2}/.test(texto)) 
+            throw new Error(`Deve estar no formato aaaa-mm-dd`);
+        
+        return Date(...texto.split('-').map((item, indice) => item - indice % 2));
+        
+    };
+
+
+};
