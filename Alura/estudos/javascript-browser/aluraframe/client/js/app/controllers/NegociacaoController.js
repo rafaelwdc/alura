7a98e25eadb17f92c1bfc8191522101decdbbafe
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-08-13 19:05:32 -0300

    vida inteligente na madrugada

--- Alura/estudos/javascript-browser/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Alura/estudos/javascript-browser/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,22 +4,30 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     };
 
     adiciona(event) {
         event.preventDefault();
-        
-        let negociacao = new Negociacao(
-            DateHelper.textoParaDate(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value
-            );
-
-            console.log(negociacao);
-            console.log(DateHelper.dataParaTexto(negociacao.data));
-            
-        };
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+        console.log(this._listaNegociacoes.negociacoes);
     };
+        
+    _criaNegociacao(){
+        return new Negociacao(
+        DateHelper.textoParaDate(this._inputData.value),
+        this._inputQuantidade.value,
+        this._inputValor.value);
+    };
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    };
+};
     
     
     
