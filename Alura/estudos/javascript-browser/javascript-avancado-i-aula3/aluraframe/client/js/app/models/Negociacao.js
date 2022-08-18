92c8020beeb1c42100ce9ce329154df64d3c2aa6
Rafael Matheus de Freitas <106930852+rafaelwdc@users.noreply.github.com>
2022-08-18 10:00:25 -0300

    backup

---
+++ Alura/estudos/javascript-browser/javascript-avancado-i-aula3/aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,24 @@
+class Negociacao {
+    constructor(data,quantidade,valor) {
+        this._data = new Date(this.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+    };
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+        return this._data;
+    };
+
+    get quantidade() {
+        return this._quantidade;
+    };
+
+    get valor() {
+        return this._valor;
+    };
+};
