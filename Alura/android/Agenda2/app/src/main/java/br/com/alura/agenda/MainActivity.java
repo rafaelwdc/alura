83ec0edb691f498422b1e3bb429189fcb3bee37f
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-08-27 19:04:33 -0300

    update no projeto android

--- Alura/android/Agenda2/app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ Alura/android/Agenda2/app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -2,9 +2,11 @@ package br.com.alura.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.TextView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ListView;
+
 import androidx.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,12 +18,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         List<String> alunos = new ArrayList<>(
-                Arrays.asList("Alex", "Fran", "Jose"));
-        TextView primeiroAluno = findViewById((R.id.textView));
-        TextView segundoAluno = findViewById((R.id.textView2));
-        TextView terceiroAluno = findViewById((R.id.textView3));
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText(alunos.get(2));
+                Arrays.asList("Rafael", "Patricky", "Camila", "Mãe", "Veio"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<> (
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos));
     }
 }
--- Alura/testes/teste007/loja-instrumentos-musicais/index.js
+++ Alura/testes/teste007/loja-instrumentos-musicais/index.js
@@ -3,7 +3,7 @@ function exibirMensagem(texto) {
 };
 
 function venda(funcionario,instrumento,cliente) {
-    exibirMensagem(`Compra efetuada com sucesso, ${instrumento.tipo}, da marca ${instrumento.fabricante}, com o modelo ${instrumento.formatoDoCorpo}, comprado pelo ${cliente.nome}, e vendido pelo vendedor: ${funcionario.nome}\n\n--[ #### Nota Fiscal #### ]--\n--Instrumentos BrosFreitas --\n-//--//--//--//--//--//--//--\n-- Cliente: ${cliente.nome} --\n-- E-mail: ${cliente.email} --\n-- Pagamento: ${cliente.formaDePagamento} --\n-- `)
+    exibirMensagem(`Compra efetuada com sucesso, ${instrumento.tipo}, da marca ${instrumento.fabricante}, com o modelo ${instrumento.formatoDoCorpo}, comprado pelo ${cliente.nome}, e vendido pelo vendedor: ${funcionario.nome}\n\n--[ #### Nota Fiscal #### ]--\n--Instrumentos BrosFreitas --\n-//--//--//--//--//--//--//--\n-- Cliente: ${cliente.nome} --\n-- E-mail: ${cliente._email} --\n-- Pagamento: ${cliente.formaDePagamento} --\n-- `)
 };
 
 
@@ -16,7 +16,7 @@ import Cliente from "./usuarios/Cliente.js";
 const instrumento = new Cordas('LDT','EDP','Guitarra LDT Prata com captação de dois Humbuckers e escala completa com 24 casas','R$1499,99','Guitarra','6 Cordas','Escala Standart com 24 casas','Les Paul','2 Humbuckers ponte/escala');
 
 const cliente = new Cliente('Clienteson','XXX.XXX.XXX-XX','cliente@loja.com','Cartão de Credito')
-const funcionario = new Funcionario('Funcionarioson','func','1234');
+const funcionario = new Funcionario('Funcionarioson','XXX.XXX.XXX-XX','funcionario@loja.com','func','1234');
 const admin = new Admin('Adminson','XXX.XXX.XXX-XX','cliente@loja.com','admin','admin');
 
 exibirMensagem(cliente);
