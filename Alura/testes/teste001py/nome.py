24eb854b316d270000ae316dc423ac88c4557dd5
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-02 19:31:56 -0300

    update video 1 / aula 1 / android 3

---
+++ Alura/testes/teste001py/nome.py
@@ -0,0 +1,5 @@
+nome = str(input('Qual seu nome ? ')).title()
+print('Que nome bonito!')
+print('Muito prazer em te conhecer, {}.'.format(nome))
+if nome == 'Patricky' or 'Rafael':
+    print('Teu nome é foda táh!')
---
+++ Alura/testes/teste007/nome.py
@@ -0,0 +1,3 @@
+nome = str(input('Qual seu nome? ')).title
+print('Que nome bonito')
+print('Muito prazer em te conhecer ', nome)
