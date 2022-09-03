273d41fea1e677f75b03cd748e2654986a86b003
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-03 03:40:12 -0300

    update 1 /  video 1 / aula 2 / android 3

--- Alura/android/android001/Agenda/app/src/main/java/br/com/alura/agenda/ui/adapter/ListaAlunosAdapter.java
+++ Alura/android/android001/Agenda/app/src/main/java/br/com/alura/agenda/ui/adapter/ListaAlunosAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,10 +37,15 @@ public class ListaAlunosAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(int posicao, View view, ViewGroup viewGroup) {
         View viewCriada = LayoutInflater
                 .from(context)
                 .inflate(R.layout.item_aluno, viewGroup, false);
+        Aluno alunoDevolvido = alunos.get(posicao);
+        TextView nome = viewCriada.findViewById(R.id.item_aluno_nome);
+        nome.setText(alunoDevolvido.getNome());
+        TextView telefone = viewCriada.findViewById(R.id.item_aluno_telefone);
+        telefone.setText(alunoDevolvido.getTelefone());
         return viewCriada;
     }
 
@@ -48,10 +54,12 @@ public class ListaAlunosAdapter extends BaseAdapter {
     }
 
     public void addAll(List<Aluno> alunos) {
+        notifyDataSetChanged();
         this.alunos.addAll(alunos);
     }
 
     public void remove(Aluno aluno) {
+        notifyDataSetChanged();
         alunos.remove(aluno);
     }
 }
