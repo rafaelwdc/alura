173c7a34d8e997a026bbc71bde680b64281e0f10
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-06 04:11:53 -0300

    v1/a3/a4

--- Alura/android/AluraViagens2/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ Alura/android/AluraViagens2/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -9,6 +9,6 @@ public class ListaPacotesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_pacotes);
     }
 }
