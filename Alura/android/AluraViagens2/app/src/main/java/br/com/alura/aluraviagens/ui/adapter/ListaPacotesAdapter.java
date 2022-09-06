793946c0ea1cf761abf3d6a45a045176469e46aa
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-06 10:28:36 -0300

    v2/a5/a4

---
+++ Alura/android/AluraViagens2/app/src/main/java/br/com/alura/aluraviagens/ui/adapter/ListaPacotesAdapter.java
@@ -0,0 +1,69 @@
+package br.com.alura.aluraviagens.ui.adapter;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.ui.activity.R;
+
+public class ListaPacotesAdapter extends BaseAdapter {
+
+    private final List<Pacote> pacotes;
+    private Context context;
+
+    public ListaPacotesAdapter(List<Pacote> pacotes, Context context) {
+        this.pacotes = pacotes;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return pacotes.size();
+    }
+
+    @Override
+    public Pacote getItem(int posicao) {
+        return pacotes.get(posicao);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int posicao, View view, ViewGroup parent) {
+        View viewCriada = LayoutInflater.from(context)
+                .inflate(R.layout.item_pacote, parent, false);
+
+        Pacote pacote = pacotes.get(posicao);
+
+        TextView local = viewCriada.findViewById(R.id.item_pacote_local);
+        local.setText(pacote.getLocal());
+
+        ImageView imagem = viewCriada.findViewById(R.id.item_pacote_imagem);
+        Resources resources = context.getResources();
+        int idDoDrawable = resources.getIdentifier(pacote.getImagem()
+                , "drawable"
+                , context.getPackageName());
+        Drawable drawableImagemPacote = resources.getDrawable(idDoDrawable);
+        imagem.setImageDrawable(drawableImagemPacote);
+
+        TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
+        dias.setText(pacote.getDias() + " dias");
+
+        TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
+        preco.setText(pacote.getPreco().toString());
+
+        return viewCriada;
+    }
+}
