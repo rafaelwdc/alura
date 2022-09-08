1ad62b505c3cd3e92e7c0f67984855ffcc863041
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-08 06:01:04 -0300

    v2.a4.ca5

--- Alura/android/AluraViagens.apk/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoCompraActivity.java
+++ Alura/android/AluraViagens.apk/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoCompraActivity.java
@@ -2,13 +2,48 @@ package br.com.alura.aluraviagens.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+
+import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.util.DataUtil;
+import br.com.alura.aluraviagens.util.MoedaUtil;
+import br.com.alura.aluraviagens.util.ResourceUtil;
 
 public class ResumoCompraActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Resumo da Compra";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_compra);
+
+        Pacote pacoteSaoPaulo = new Pacote(
+                "São Paulo",
+                "sao_paulo_sp",
+                2,
+                new BigDecimal("243.99"));
+
+        TextView local = findViewById(R.id.resumo_compra_local_pacote);
+        local.setText(pacoteSaoPaulo.getLocal());
+
+        ImageView imagem = findViewById(R.id.resumo_compra_imagem_pacote);
+        Drawable drawableDoPacote = ResourceUtil.devolveDrawbable(this, pacoteSaoPaulo.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+
+        TextView data = findViewById(R.id.resumo_compra_data_viagem);
+        String periodoEmTexto = DataUtil.periodoEmTexto(pacoteSaoPaulo.getDias());
+        data.setText(periodoEmTexto);
+
+        TextView preco = findViewById(R.id.resumo_compra_preco_pacote);
+        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacoteSaoPaulo.getPreco());
+        preco.setText(moedaBrasileira);
+
+        setTitle(TITULO_APPBAR);
     }
 }
