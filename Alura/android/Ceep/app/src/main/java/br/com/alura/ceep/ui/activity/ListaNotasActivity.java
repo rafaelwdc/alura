84027754c985a000aa320c85f3f92a744acc1ece
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-09 11:16:13 -0300

    v4.a1.ca6

--- Alura/android/Ceep/app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ Alura/android/Ceep/app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -1,10 +1,16 @@
 package br.com.alura.ceep.ui.activity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 import br.com.alura.ceep.R;
+import br.com.alura.ceep.dao.NotaDAO;
+import br.com.alura.ceep.model.Nota;
+import br.com.alura.ceep.ui.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -12,5 +18,17 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listaNotas = findViewById(R.id.listView);
+
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira nota",
+                "Primeira descrição"));
+
+        List<Nota> todasNotas = dao.todos();
+
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
+
+
     }
 }
