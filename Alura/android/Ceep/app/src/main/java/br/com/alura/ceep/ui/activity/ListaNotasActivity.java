022e48c4aca76ddbd703e1dc3a5d2cd76788cd8c
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-10 06:10:15 -0300

    completa.a2.ca6

--- Alura/android/Ceep/app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ Alura/android/Ceep/app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -3,6 +3,7 @@ package br.com.alura.ceep.ui.activity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -23,17 +24,14 @@ public class ListaNotasActivity extends AppCompatActivity {
         RecyclerView listaNotas = findViewById(R.id.lista_notas_recyclerview);
 
         NotaDAO dao = new NotaDAO();
-        for (int i = 1;i <= 10000; i++) {
+        for (int i = 1; i <= 10000; i++) {
             dao.insere(new Nota("Titulo " + i,
                     "Descrição " + i));
         }
-
-
         List<Nota> todasNotas = dao.todos();
 
-        listaNotas.setAdapter(new ListaNotasAdapters());
-
-
-
+        listaNotas.setAdapter(new ListaNotasAdapters(this, todasNotas));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        listaNotas.setLayoutManager(layoutManager);
     }
 }
--- Alura/android/Ceep/app/src/main/java/br/com/alura/ceep/ui/recyclerview/activity/ListaNotasAdapters.java
+++ Alura/android/Ceep/app/src/main/java/br/com/alura/ceep/ui/recyclerview/activity/ListaNotasAdapters.java
@@ -1,36 +1,57 @@
 package br.com.alura.ceep.ui.recyclerview.activity;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
 
+import br.com.alura.ceep.R;
 import br.com.alura.ceep.model.Nota;
 import br.com.alura.ceep.ui.adapter.ListaNotasAdapter;
 
 public class ListaNotasAdapters extends RecyclerView.Adapter {
 
     private List<Nota> notas;
+    private Context context;
 
-    public ListaNotasAdapters(List<Nota> notas) {
+    public ListaNotasAdapters(Context context, List<Nota> notas) {
+        this.context = context;
         this.notas = notas;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_nota, parent, false);
+        return new NotaViewHolder(viewCriada);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+        Nota nota = notas.get(position);
+        TextView titulo = holder.itemView.findViewById(R.id.item_nota_titulo);
+        titulo.setText(nota.getTitulo());
+        TextView descricao = holder.itemView.findViewById(R.id.item_nota_descricao);
+        descricao.setText(nota.getDescricao());
     }
 
     @Override
     public int getItemCount() {
         return notas.size();
+    }
+
+    class NotaViewHolder extends RecyclerView.ViewHolder {
+
+        public NotaViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 }
