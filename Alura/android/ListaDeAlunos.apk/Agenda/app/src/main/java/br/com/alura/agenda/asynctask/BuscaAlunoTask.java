1f38ec0aa4e2898b275c1ce13de39798c80fd574
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-17 00:03:26 -0300

    v3.a4.ca9

--- Alura/android/ListaDeAlunos.apk/Agenda/app/src/main/java/br/com/alura/agenda/asynctask/BuscaAlunoTask.java
+++ Alura/android/ListaDeAlunos.apk/Agenda/app/src/main/java/br/com/alura/agenda/asynctask/BuscaAlunoTask.java
@@ -1,11 +1,15 @@
 package br.com.alura.agenda.asynctask;
 
 import android.os.AsyncTask;
+import android.os.Trace;
+
+import java.util.List;
 
 import br.com.alura.agenda.database.dao.AlunoDAO;
+import br.com.alura.agenda.model.Aluno;
 import br.com.alura.agenda.ui.adapter.ListaAlunosAdapter;
 
-public class BuscaAlunoTask extends AsyncTask {
+public class BuscaAlunoTask extends AsyncTask<Void, Void, List<Aluno>> {
     private final AlunoDAO dao;
     private final ListaAlunosAdapter adapter;
 
@@ -16,8 +20,13 @@ public class BuscaAlunoTask extends AsyncTask {
     }
 
     @Override
-    protected Object doInBackground(Object[] objects) {
-        adapter.atualiza(dao.todos());
-        return null;
+    protected List<Aluno> doInBackground(Void[] objects) {
+        return dao.todos();
+    }
+
+    @Override
+    protected void onPostExecute(List<Aluno> todosAlunos) {
+        super.onPostExecute(todosAlunos);
+        adapter.atualiza((List<Aluno>) todosAlunos);
     }
 }
