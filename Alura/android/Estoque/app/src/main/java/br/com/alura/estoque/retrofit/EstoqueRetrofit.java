31d0842dfac4237d5a0d6de6a95c489eb4b7a15f
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-20 23:17:49 -0300

    finalizada a2.ca10

--- Alura/android/Estoque/app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
+++ Alura/android/Estoque/app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -1,7 +1,8 @@
 package br.com.alura.estoque.retrofit;
 
-import br.com.alura.estoque.model.Produto;
 import br.com.alura.estoque.retrofit.service.ProdutoService;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,8 +11,15 @@ public class EstoqueRetrofit {
     private final ProdutoService produtoService;
 
     public EstoqueRetrofit() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://172.22.160.1:8080/")
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         produtoService = retrofit.create(ProdutoService.class);
