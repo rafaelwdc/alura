9ce7de7e6bf7668b25f747cb014854fe2e91e700
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-20 21:58:39 -0300

    v2.a2.ca10

--- Alura/android/Estoque/app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
+++ Alura/android/Estoque/app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -1,10 +1,23 @@
 package br.com.alura.estoque.retrofit;
 
+import br.com.alura.estoque.model.Produto;
+import br.com.alura.estoque.retrofit.service.ProdutoService;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class EstoqueRetrofit {
 
+    private final ProdutoService produtoService;
+
     public EstoqueRetrofit() {
-        new Retrofit.Builder().baseUrl("http://172.22.160.1:8080/produto");
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://172.22.160.1:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        produtoService = retrofit.create(ProdutoService.class);
+    }
+
+    public ProdutoService getProdutoService() {
+        return produtoService;
     }
 }
