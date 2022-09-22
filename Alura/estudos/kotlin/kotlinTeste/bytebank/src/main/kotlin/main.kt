679eb66f3786d4ae7bac0c690b67de0010b09f7d
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-22 20:10:49 -0300

    finalizada.a1.ck001

--- Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
+++ Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
@@ -1,6 +1,12 @@
 fun main() {
     println("Bem Vindo ao ByteBank")
-    val titular = "Rafael"
+    val titular: String = "Rafael"
+    val numerConta: Int = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
 
     println("Titular: $titular")
+    println("Numero da Conta: $numerConta")
+    println("Saldo da Conta: $saldo")
 }
