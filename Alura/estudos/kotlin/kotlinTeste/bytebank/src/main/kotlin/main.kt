f979241b4dd2c5517f12bb7fd1083df585b6c341
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-23 04:35:49 -0300

    v2.a2.ck001

--- Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
+++ Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
@@ -1,28 +1,53 @@
 fun main() {
+
     println("Bem Vindo ao ByteBank")
-    val titular: String = "Rafael"
-    val numerConta: Int = 1000
-    var saldo = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
-    saldo -= 1000.0
 
-    println("Titular: $titular")
-    println("Numero da Conta: $numerConta")
-    println("Saldo da Conta: $saldo")
+    var i = 0
+    while (i  <  5) {
+        val titular: String = "Rafael $i"
+        val numerConta: Int = 1000 + i
+        var saldo = i + 10.0
 
-    if(saldo > 0.0) {
+        println("Titular: $titular")
+        println("Numero da Conta: $numerConta")
+        println("Saldo da Conta: $saldo")
+        println("")
+        i++
+
+    }
+
+
+
+
+
+
+
+//    for (i in 5 downTo 1 step 5) {
+//
+
+//
+//    }
+//        testaCondicoes(saldo)
+
+}
+
+fun testaCondicoes(saldo: Double) {
+
+    if (saldo > 0.0) {
         println("Conta é Positiva")
-    }else if (saldo == 0.0) {
+    } else if (saldo == 0.0) {
         println("Conta é Neutra")
     } else {
         println("Conta é negativa")
     }
 
-    when {
-        saldo > 0.0 -> println("Conta é positiva")
-        saldo == 0.0 -> println("Conta é Neutra")
-        else -> println("Conta é negativa")
-    }
-
 }
+
+
+
+//    when {
+//        saldo > 0.0 -> println("Conta é positiva")
+//        saldo == 0.0 -> println("Conta é Neutra")
+//        else -> println("Conta é negativa")
+//
+//    }
