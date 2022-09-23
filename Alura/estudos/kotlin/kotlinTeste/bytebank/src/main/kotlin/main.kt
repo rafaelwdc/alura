88b8c34a8563c0c4cb98c3a1cc5e60c71e12b859
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-23 11:03:00 -0300

    v2.a3.ck001

--- Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
+++ Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
@@ -1,7 +1,51 @@
 fun main() {
-
     println("Bem Vindo ao ByteBank")
 
+    // Conta Rafael
+    val contaRafael = Conta()
+    contaRafael.titular = "Rafael"
+    contaRafael.numero = 1000
+    contaRafael.saldo = 200.0
+
+
+    // Conta Patricky
+    val contaPatricky = Conta()
+    contaPatricky.titular = "Patricky"
+    contaPatricky.numero = 1001
+    contaPatricky.saldo = 300.0
+
+    // Prints
+    println(contaRafael.titular)
+    println(contaRafael.numero)
+    println(contaRafael.saldo)
+
+    println(contaPatricky.titular)
+    println(contaPatricky.numero)
+    println(contaPatricky.saldo)
+
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("NumeroX $numeroX")
+    println("NumeroY $numeroY")
+
+    val contaCamila = Conta()
+    contaCamila.titular = "Camila"
+    var contaHirode = contaCamila
+
+    println("Titular conta Camila: ${contaCamila.titular}")
+
+
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun testaLacos() {
     var i = 0
     while (i  <  5) {
         val titular: String = "Rafael $i"
@@ -15,20 +59,18 @@ fun main() {
         i++
     }
 
-//    for (i in 5 downTo 1 step 5) {
-//
-//    val titular: String = "Rafael $i"
-//    val numerConta: Int = 1000 + i
-//    var saldo = i + 10.0
-//
-//    println("Titular: $titular")
-//    println("Numero da Conta: $numerConta")
-//    println("Saldo da Conta: $saldo")
-//    println("")
-//
-//    }
-//        testaCondicoes(saldo)
+    for (i in 5 downTo 1 step 5) {
 
+        val titular: String = "Rafael $i"
+        val numerConta: Int = 1000 + i
+        var saldo = i + 10.0
+
+        println("Titular: $titular")
+        println("Numero da Conta: $numerConta")
+        println("Saldo da Conta: $saldo")
+        println("")
+
+    }
 }
 
 fun testaCondicoes(saldo: Double) {
