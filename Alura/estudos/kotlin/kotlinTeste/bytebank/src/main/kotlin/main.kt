24b27d96bd43c609717fdca69ca40242f5913bfa
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-23 06:09:36 -0300

    vida inteligente na madrudada

--- Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
+++ Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
@@ -13,18 +13,18 @@ fun main() {
         println("Saldo da Conta: $saldo")
         println("")
         i++
-
     }
-
-
-
-
-
-
 
 //    for (i in 5 downTo 1 step 5) {
 //
-
+//    val titular: String = "Rafael $i"
+//    val numerConta: Int = 1000 + i
+//    var saldo = i + 10.0
+//
+//    println("Titular: $titular")
+//    println("Numero da Conta: $numerConta")
+//    println("Saldo da Conta: $saldo")
+//    println("")
 //
 //    }
 //        testaCondicoes(saldo)
