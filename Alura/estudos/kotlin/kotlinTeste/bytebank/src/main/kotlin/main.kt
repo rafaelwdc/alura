5af2f9fff082db90eb7f3dfa18afcc4a0222ebdf
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-22 21:45:45 -0300

    v1.a2.ck001

--- Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
+++ Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
@@ -5,8 +5,24 @@ fun main() {
     var saldo = 0.0
     saldo = 100 + 2.0
     saldo += 200
+    saldo -= 1000.0
 
     println("Titular: $titular")
     println("Numero da Conta: $numerConta")
     println("Saldo da Conta: $saldo")
+
+    if(saldo > 0.0) {
+        println("Conta é Positiva")
+    }else if (saldo == 0.0) {
+        println("Conta é Neutra")
+    } else {
+        println("Conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("Conta é positiva")
+        saldo == 0.0 -> println("Conta é Neutra")
+        else -> println("Conta é negativa")
+    }
+
 }
