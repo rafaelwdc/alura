7d96d0b1453452f2e6fe5e850f2207e2bc38ef88
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-24 05:17:00 -0300

    finalizada.ck001

--- Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
+++ Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
@@ -143,11 +143,3 @@ fun testaCondicoes(saldo: Double) {
     }
 
 }
-
-
-//    when {
-//        saldo > 0.0 -> println("Conta é positiva")
-//        saldo == 0.0 -> println("Conta é Neutra")
-//        else -> println("Conta é negativa")
-//
-//    }
