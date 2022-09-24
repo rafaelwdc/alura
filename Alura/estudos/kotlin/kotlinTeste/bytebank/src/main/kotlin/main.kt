c874ee14add3ea84135aa5f44f42f5aa5eb8d72c
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-24 04:16:57 -0300

    finalizada.a4.ck001

--- Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
+++ Alura/estudos/kotlin/kotlinTeste/bytebank/src/main/kotlin/main.kt
@@ -5,58 +5,58 @@ fun main() {
     val contaRafael = Conta()
     contaRafael.titular = "Rafael"
     contaRafael.numero = 1000
-    contaRafael.saldo = 200.0
+    contaRafael.setSaldo(-200.0)
 
     // Conta Patricky
     val contaPatricky = Conta()
     contaPatricky.titular = "Patricky"
     contaPatricky.numero = 1001
-    contaPatricky.saldo = 300.0
+    contaPatricky.setSaldo(300.0)
 
     // Prints
     println(contaRafael.titular)
     println(contaRafael.numero)
-    println(contaRafael.saldo)
+    println(contaRafael.getSaldo())
 
     println(contaPatricky.titular)
     println(contaPatricky.numero)
-    println(contaPatricky.saldo)
+    println(contaPatricky.getSaldo())
 
     // Comportamentos / Metodos
-    println("Depositando na conta do ${contaRafael.titular}")
-    contaRafael.deposita(50.0)
-    println(contaRafael.saldo)
-    println("Depositando na conta do ${contaPatricky.titular}")
-    contaPatricky.deposita(70.0)
-    println(contaPatricky.saldo)
-
-    println("Sacando da conta do ${contaRafael.titular}")
-    contaRafael.saca(250.0)
-    println(contaRafael.saldo)
-
-    println("Sacando da conta do ${contaPatricky.titular}")
-    contaPatricky.saca(100.0)
-    println(contaPatricky.saldo)
-
-
-    println("Saque em Excesso na conta do titular: ${contaRafael.titular}")
-    contaRafael.saca(100.0)
-    println(contaRafael.saldo)
-
-    println("Saque em Excesso na conta do titular:  ${contaPatricky.titular} ")
-    contaRafael.saca(500.0)
-    println(contaRafael.saldo)
-
-    println("Trasnferencia da conta de ${contaPatricky.titular} para a conta de ${contaRafael.titular}")
-
-    if(contaPatricky.transfere(300.0,contaRafael)) {
-        println("Transferência sucessedida")
-    } else {
-        println("Falha na TransferÊncia")
-    }
-
-    println("Saldo da conta do ${contaRafael.titular}:  ${contaRafael.saldo} ")
-    println("Saldo da conta do ${contaPatricky.titular}:  ${contaPatricky.saldo} ")
+//    println("Depositando na conta do ${contaRafael.titular}")
+//    contaRafael.deposita(50.0)
+//    println(contaRafael.getSaldo())
+//    println("Depositando na conta do ${contaPatricky.titular}")
+//    contaPatricky.deposita(70.0)
+//    println(contaPatricky.getSaldo())
+//
+//    println("Sacando da conta do ${contaRafael.titular}")
+//    contaRafael.saca(250.0)
+//    println(contaRafael.getSaldo())
+//
+//    println("Sacando da conta do ${contaPatricky.titular}")
+//    contaPatricky.saca(100.0)
+//    println(contaPatricky.getSaldo())
+//
+//
+//    println("Saque em Excesso na conta do titular: ${contaRafael.titular}")
+//    contaRafael.saca(100.0)
+//    println(contaRafael.getSaldo())
+//
+//    println("Saque em Excesso na conta do titular:  ${contaPatricky.titular} ")
+//    contaRafael.saca(500.0)
+//    println(contaRafael.getSaldo())
+//
+//    println("Trasnferencia da conta de ${contaPatricky.titular} para a conta de ${contaRafael.titular}")
+//
+//    if(contaPatricky.transfere(300.0,contaRafael)) {
+//        println("Transferência sucessedida")
+//    } else {
+//        println("Falha na TransferÊncia")
+//    }
+//
+//    println("Saldo da conta do ${contaRafael.titular}:  ${contaRafael.getSaldo()} ")
+//    println("Saldo da conta do ${contaPatricky.titular}:  ${contaPatricky.getSaldo()} ")
 
 
 }
@@ -66,7 +66,7 @@ class Conta {
     // Atributos da Classe
     var titular = ""
     var numero = 0
-    var saldo = 0.0
+    private var saldo = 0.0
 
     // Comportamentos da Classe
     fun deposita(valor: Double) {
@@ -82,10 +82,20 @@ class Conta {
     fun transfere(valor: Double, destino: Conta): Boolean {
         if (saldo >= valor) {
             saldo -= valor
-            destino.saldo += valor
+            destino.deposita(valor)
             return true
         }
         return false
+    }
+
+    fun getSaldo(): Double {
+        return saldo
+    }
+
+    fun setSaldo(valor: Double) {
+        if (valor > 0) {
+            saldo = valor
+        }
     }
 }
 
