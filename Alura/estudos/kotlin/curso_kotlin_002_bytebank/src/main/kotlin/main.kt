4f14392a2b5a2310b42c07ea43763a5b61a75cd6
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-24 19:25:34 -0300

    v3.a1.ck002

--- Alura/estudos/kotlin/curso_kotlin_002_bytebank/src/main/kotlin/main.kt
+++ Alura/estudos/kotlin/curso_kotlin_002_bytebank/src/main/kotlin/main.kt
@@ -1,4 +1,16 @@
 fun main() {
     println("Bem Vindo ao ByteBank")
-    testaComportamentosConta()
+
+    val rafa = Funcionario(
+        nome = "Rafael",
+        cpf = "898.456.321.59",
+        salario = 1000.0
+    )
+
+    println("Nome: ${rafa.nome}")
+    println("CPF: ${rafa.cpf}")
+    println("Salário: ${rafa.salario}")
+
+    println("Bonificação: ${rafa.bonificacao()}")
+
 }
