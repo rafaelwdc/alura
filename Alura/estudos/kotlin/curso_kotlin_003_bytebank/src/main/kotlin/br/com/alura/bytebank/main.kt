509516b5e5720c0c9fe65b289377fc8f58405278
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-09-30 15:58:00 -0300

    v2.a3.ck003

--- Alura/estudos/kotlin/curso_kotlin_003_bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ Alura/estudos/kotlin/curso_kotlin_003_bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,10 +1,20 @@
-import br.com.alura.bytebank.modelo.Cliente
-import br.com.alura.bytebank.modelo.ContaCorrente
-import br.com.alura.bytebank.modelo.ContaPoupanca
-import br.com.alura.bytebank.modelo.totalContas
+import br.com.alura.bytebank.modelo.*
 
 
 fun main() {
+
+    val patricky = object: Autenticavel {
+        val nome: String = "Patricky"
+        val cpf: String = "xxx.xxx.xxx-xx"
+        val senha: Int = 1234
+
+        override fun autentica(senha: Int) = this.senha == senha
+    }
+
+    val sistemaInterno = SistemaInterno()
+    sistemaInterno.entra(login = patricky, senha = 1234)
+
+    println("Nome do Cliente ${patricky.nome}")
 
     val rafael = Cliente(nome = "Rafael", cpf = "", senha = 1)
     val contaPolpanca = ContaPoupanca(titular = rafael, numero = 1000)
--- Alura/estudos/kotlin/curso_kotlin_003_bytebank/src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
+++ Alura/estudos/kotlin/curso_kotlin_003_bytebank/src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
@@ -2,8 +2,8 @@ package br.com.alura.bytebank.modelo
 
 class SistemaInterno {
 
-    fun entra(admin: Autenticavel, senha: Int) {
-        if (admin.autentica(senha)) {
+    fun entra(login: Autenticavel, senha: Int) {
+        if (login.autentica(senha)) {
             println("Bem vindo ao Bytebank")
         } else {
             println("Falha na autenticação")
