98b430137998cd44dc478943499f08872482e336
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-02 23:17:12 -0300

    finalizada.a1.ck004

--- Alura/estudos/kotlin/curso_kotlin_004_bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ Alura/estudos/kotlin/curso_kotlin_004_bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,7 +1,8 @@
-import br.com.alura.bytebank.teste.*
+import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
     println("Inicio Main")
+    10/0
     funcao1()
     println("Fim Main")
 }
@@ -15,6 +16,8 @@ fun funcao2 () {
     println("Inicio da Função 2")
     for(i in 1..5) {
         println(i)
+        val endereco = Any()
+        endereco as Endereco
     }
     println("Fim da Função 2")
 }
