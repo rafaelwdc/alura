85e3fbf7529b442a9b972c0717a7da6f3ec81232
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-03 15:36:54 -0300

    v2.a5.ck004

--- Alura/estudos/kotlin/curso_kotlin_004_bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ Alura/estudos/kotlin/curso_kotlin_004_bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,10 +1,11 @@
-import br.com.alura.bytebank.exception.SaldoInsuficienteException
 import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    var enderecoNulo : Endereco? = null
-    val enderecoNaoNulo : Endereco = enderecoNulo!!
-    enderecoNaoNulo.logradouro
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+    enderecoNulo?.let { endereco ->
+        println(endereco.logradouro.length)
+    }
 }
 
 
