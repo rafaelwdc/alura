d381ffa4fdcf4bba0e1312ccb3d9e2daa6c086ed
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-03 10:46:25 -0300

    v1.a2.ck004

--- Alura/estudos/kotlin/curso_kotlin_004_bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ Alura/estudos/kotlin/curso_kotlin_004_bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,24 +1,31 @@
 import br.com.alura.bytebank.modelo.Endereco
+import java.lang.ClassCastException
 
 fun main() {
     println("Inicio Main")
-    10/0
     funcao1()
     println("Fim Main")
 }
+
 fun funcao1() {
     println("Inicio Função 1")
-    funcao2()
+    try {
+        funcao2()
+    } catch (e: ClassCastException) {
+        println("ClassCastException foi pegado")
+    }
     println("Fim da Função 1")
 }
 
-fun funcao2 () {
+fun funcao2() {
     println("Inicio da Função 2")
-    for(i in 1..5) {
+
+    for (i in 1..5) {
         println(i)
         val endereco = Any()
         endereco as Endereco
     }
+
     println("Fim da Função 2")
 }
 
