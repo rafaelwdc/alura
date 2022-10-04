491136f17f293b33f9b9b76a7c01dbe39a785bb9
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-04 10:05:32 -0300

    v2.a1.ck005

--- Alura/estudos/kotlin/curso_kotlin_005_array/array/src/main/kotlin/br/com/alura/array/Main.kt
+++ Alura/estudos/kotlin/curso_kotlin_005_array/array/src/main/kotlin/br/com/alura/array/Main.kt
@@ -1,26 +1,20 @@
 fun main() {
-    val idade1 = 25
-    val idade2 = 19
-    val idade3 = 33
-    val idade4 = 20
+    val idades: IntArray = intArrayOf(25, 19, 33, 20, 55, 40)
 
-    val maiorIdade = if (idade1 > idade2 && idade1 > idade3 &&  idade1 > idade4) {
-        idade1
-    } else if (idade2 > idade3 && idade2 > idade4) {
-        idade2
-    } else if (idade3 > idade4){
-        idade3
-    } else {
-        idade4
+    var maiorIdade = Int.MIN_VALUE
+    for (idade in idades) {
+        if (idade > maiorIdade) {
+            maiorIdade = idade
+        }
     }
 
     println(maiorIdade)
 
-    val idades = IntArray(4)
-    idades[0] = 25
-    idades[1] = 19
-    idades[2] = 33
-    idades[3] = 20
-
-    
+    var menorIdade = Int.MAX_VALUE
+    idades.forEach { idade ->
+        if(idade < menorIdade) {
+            menorIdade = idade
+        }
+    }
+    println(menorIdade)
 }
