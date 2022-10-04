b565c1c165b43e05651e2fa6e6e5d3d81364d350
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-04 12:14:16 -0300

    v2.a3.ck005

--- Alura/estudos/kotlin/curso_kotlin_005_array/array/src/main/kotlin/br/com/alura/array/Main.kt
+++ Alura/estudos/kotlin/curso_kotlin_005_array/array/src/main/kotlin/br/com/alura/array/Main.kt
@@ -10,19 +10,38 @@ fun main() {
     val aumento = "1.1".toBigDecimal()
 
     val salariosComAumento: Array<BigDecimal> = salarios
-        .map { salario ->
-            if(salario < "5000".toBigDecimal()) {
-                salario + "500".toBigDecimal()
-            } else {
-                (salario * aumento).setScale(2, RoundingMode.UP)
-            }
-        }
+        .map { salario -> calculaAumentoRelativo(salario, aumento) }
         .toTypedArray()
 
     println(salariosComAumento.contentToString())
+    val gastoInicial = salariosComAumento.somatoria()
+    println("Gasto de 1 mes de salarios para todos funcionarios: $gastoInicial")
+
+    val meses = 6.toBigDecimal()
+    val gastoTotal = salariosComAumento.fold(gastoInicial) { acumulador, salario ->
+        acumulador + (salario * meses).setScale(2, RoundingMode.UP)
+    }
+    println("Gasto Total durante 6 meses: $gastoTotal")
+
 }
+
+private fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal):
+        BigDecimal {
+    return if (salario < "5000".toBigDecimal()) {
+        salario + "500".toBigDecimal()
+    } else {
+        (salario * aumento).setScale(2, RoundingMode.UP)
+    }
+}
+
 fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
     return Array<BigDecimal>(valores.size) { i ->
         valores[i].toBigDecimal()
+    }
+}
+
+fun Array<BigDecimal>.somatoria(): BigDecimal {
+    return this.reduce { acumulador, valor ->
+        acumulador + valor
     }
 }
