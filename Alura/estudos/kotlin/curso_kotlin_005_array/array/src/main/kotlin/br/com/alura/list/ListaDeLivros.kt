248de5f909f23a1f0a0db4cc05eb0f6dd596c802
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-04 14:20:16 -0300

    finalizada.a4.ck005

--- Alura/estudos/kotlin/curso_kotlin_005_array/array/src/main/kotlin/br/com/alura/list/ListaDeLivros.kt
+++ Alura/estudos/kotlin/curso_kotlin_005_array/array/src/main/kotlin/br/com/alura/list/ListaDeLivros.kt
@@ -1,49 +1,30 @@
 package br.com.alura.list
 
 fun main() {
-    val livro1 = Livro(
-        titulo = "Grande Sertão: Veredas",
-        autor = "João Guimarães Rosa",
-        anoPublicacao = 1956
-    )
-    val livro2 = Livro(
-        titulo = "Minha vida de menina",
-        autor = "Helena Morley",
-        anoPublicacao = 1942,
-        editora = "Editora A"
-    )
-    val livro3 = Livro(
-        titulo = "Memórias Póstumas de Brás Cubas",
-        autor = "Machado de Assis",
-        anoPublicacao = 1881
-    )
-    val livro4 = Livro(
-        titulo = "Iracema",
-        autor = "José de Alencar",
-        anoPublicacao = 1965,
-        editora = "Editora B"
-    )
 
-    val livros: MutableList<Livro> = mutableListOf(livro1, livro2, livro3, livro4)
-
-    livros.add(
+    listaDeLivros.add(
         Livro(
             titulo = "Sagarana",
             autor = "João Guimarães Rosa",
             anoPublicacao = 1946
         )
     )
-    livros.imprimeComMarcadores()
-    livros.remove(livro1)
-    livros.imprimeComMarcadores()
-    val ordenadoAnoPublicacao: List<Livro> = livros.sorted()
+    listaDeLivros.imprimeComMarcadores()
+    listaDeLivros.remove(listaDeLivros[1])
+    listaDeLivros.imprimeComMarcadores()
+    val ordenadoAnoPublicacao: List<Livro> = listaDeLivros.sorted()
     ordenadoAnoPublicacao.imprimeComMarcadores()
 
-    val ordenadoPorTitulo = livros.sortedBy { it.titulo }
+    val ordenadoPorTitulo = listaDeLivros.sortedBy { it.titulo }
     ordenadoAnoPublicacao.imprimeComMarcadores()
-    
-    val ordenadoPorAutor = livros.sortedBy { it.autor }
-    ordenadoAnoPublicacao.imprimeComMarcadores()
+
+    listaDeLivros.sortedBy { it.autor }.imprimeComMarcadores()
+
+    val titulos: List<String> = listaDeLivros
+        .filter { it.autor.startsWith("J") }
+        .sortedBy { it.anoPublicacao }
+        .map { it.titulo }
+    println(titulos)
 }
 
 fun List<Livro>.imprimeComMarcadores() {
---
+++ Alura/estudos/kotlin/curso_kotlin_005_array/array/src/main/kotlin/br/com/alura/list/livros.kt
@@ -0,0 +1,44 @@
+package br.com.alura.list
+
+val listaDeLivros = mutableListOf(
+    Livro(
+        titulo = "Grande Sertão: Veredas",
+        autor = "João Guimarães Rosa",
+        anoPublicacao = 1956
+    ),
+    Livro(
+        titulo = "Minha vida de menina",
+        autor = "Helena Morley",
+        anoPublicacao = 1942,
+        editora = "Editora A"
+    ),
+    Livro(
+        titulo = "Memórias Póstumas de Brás Cubas",
+        autor = "Machado de Assis",
+        anoPublicacao = 1881
+    ),
+    Livro(
+        titulo = "Iracema",
+        autor = "José de Alencar",
+        anoPublicacao = 1965,
+        editora = "Editora B"
+    ),
+    Livro(
+        titulo = "Vidas Secas",
+        autor = "Graciliano Ramos",
+        anoPublicacao = 1938,
+        editora = "Editora A"
+    ),
+    Livro(
+        titulo = "Mayobe",
+        autor = "Pepetela",
+        anoPublicacao = 1979,
+        editora = "Editora B"
+    ),
+    Livro(
+        titulo = "O Cortiço",
+        autor = "Aluísio Azevedo",
+        anoPublicacao = 1890,
+        editora = "Editora B"
+    )
+)
