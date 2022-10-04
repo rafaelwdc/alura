e05f85b6aab85f1cc4b777e6d86dcafa634e837c
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-04 15:02:46 -0300

    finalizada.ck005

--- Alura/estudos/kotlin/curso_kotlin_005_array/array/src/main/kotlin/br/com/alura/list/UsaPrateleira.kt
+++ Alura/estudos/kotlin/curso_kotlin_005_array/array/src/main/kotlin/br/com/alura/list/UsaPrateleira.kt
@@ -4,12 +4,9 @@ fun main() {
     val prateleira = Prateleira(genero = "Literatura", livros = listaDeLivros)
 
     val porAutor = prateleira.organizarPorAutor()
-
-    porAutor.imprimeComMarcadores()
-    
     val porAnoPublicacao = prateleira.organizarPorAnoPublicacao()
 
-
+    porAutor.imprimeComMarcadores()
     porAnoPublicacao.imprimeComMarcadores()
 
 }
