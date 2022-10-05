5f83cf36f889032b209b992b7a4fd44d48d360c7
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-05 16:57:45 -0300

    v2.a2.ck006

--- Alura/estudos/kotlin/curso_kotlin_006_collection/src/br/com/alura/main.kt
+++ Alura/estudos/kotlin/curso_kotlin_006_collection/src/br/com/alura/main.kt
@@ -2,10 +2,14 @@ package br.com.alura
 
 fun main() {
 
-    val assistiramCursoAndroid = listOf("Rafael", "Patricky", "Camila", "Néia", "Dori")
-    val assistiramCursoKotlin = listOf("Steelx","Debler", "Dudinha", "Hiro")
-    val assistiramAmbos = assistiramCursoAndroid + assistiramCursoKotlin
-    println(assistiramAmbos.distinct())
+    val assistiramCursoAndroid: MutableSet<String> = mutableSetOf("Rafael", "Patricky", "Camila", "Néia", "Dori")
+    val assistiramCursoKotlin: MutableSet<String> = mutableSetOf("Steelx","Debler", "Dudinha", "Hiro")
+//    val assistiramAmbos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
+    val assistiramAmbos = mutableSetOf<String>()
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Intruso")
+    println(assistiramAmbos)
 
 
 }
