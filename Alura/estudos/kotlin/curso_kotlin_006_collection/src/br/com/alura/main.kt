f821421bad987eea5072902ff6d016af46fa0ed7
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-05 18:25:09 -0300

    v1.a2.ck006

--- Alura/estudos/kotlin/curso_kotlin_006_collection/src/br/com/alura/main.kt
+++ Alura/estudos/kotlin/curso_kotlin_006_collection/src/br/com/alura/main.kt
@@ -1,35 +1,81 @@
 package br.com.alura
 
 fun main() {
-
     val pedidos: MutableMap<Int, Double> = mutableMapOf(
         Pair(1, 20.00),
-        Pair(2,34.00),
-        3 to 50.00
+        Pair(2, 64.00),
+        3 to 50.00,
+        4 to 100.00,
+        5 to 150.00,
+        6 to 80.00
     )
+    //    val valorPedido = pedidos.getValue(5)
+//    println(valorPedido)
+    val mensagem:Double? = pedidos.getOrElse(0) {
+        0.0
+    }
+
+    println(mensagem)
+    println(pedidos.getOrDefault(1, -1.0))
+    println(pedidos.getOrDefault(0, -1.0))
+
+    println(pedidos.keys)
+
+    for(numero in pedidos.keys) {
+        println("Pedido: $numero")
+    }
+    println(pedidos.values)
+
+    for (valor in pedidos.values) {
+        println("Valor do pedido: $valor")
+    }
+
+    val pedidosFiltrados = pedidos.filter { (numero: Int,valor: Double) ->
+        numero % 2 == 0 && valor > 50.0
+    }
+    
+    println(pedidosFiltrados)
+
+    val pedidosAcima = pedidos.filterValues { valor ->
+        valor > 70
+    }
+
+    println(pedidosAcima)
+
+    val pedidosPares = pedidos.filterKeys { numero ->
+        numero % 2 == 0
+    }
+
+    println(pedidosPares)
+}
+
+
+
+fun testaMap(pedidos: MutableMap<Int, Double>) {
+
     println(pedidos)
     val pedido = pedidos[1]
     pedido?.let {
         println("Pedido: $it")
     }
-    for(p: Map.Entry<Int, Double> in pedidos) {
+    for (p: Map.Entry<Int, Double> in pedidos) {
         println("Número do pedido: ${p.key}")
         println("Valor do pedido: R$ ${p.value}0")
     }
     pedidos[4] = 70.00
     println(pedidos)
-    pedidos.put(5,80.0)
+    pedidos.put(5, 80.0)
     pedidos[1] = 100.0
     println(pedidos)
-    pedidos.putIfAbsent(6,200.0)
+    pedidos.putIfAbsent(6, 200.0)
     println(pedidos)
-    pedidos.putIfAbsent(6,150.0)
+    pedidos.putIfAbsent(6, 150.0)
     println(pedidos)
 
     pedidos.remove(6)
     println(pedidos)
-    pedidos.remove(3,100.0)
+    pedidos.remove(3, 100.0)
     println(pedidos)
-    pedidos.remove(3,50.0)
+    pedidos.remove(3, 50.0)
     println(pedidos)
 }
