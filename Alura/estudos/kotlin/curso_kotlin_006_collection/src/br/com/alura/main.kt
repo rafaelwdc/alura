d1cf854c49080ade8526acef20438e8670089ff3
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-05 19:02:22 -0300

    finalizada.a4.ck006

--- Alura/estudos/kotlin/curso_kotlin_006_collection/src/br/com/alura/main.kt
+++ Alura/estudos/kotlin/curso_kotlin_006_collection/src/br/com/alura/main.kt
@@ -6,7 +6,7 @@ fun main() {
         Pair(2, 64.00),
         3 to 50.00,
         4 to 100.00,
-        5 to 150.00,
+        5 to 100.00,
         6 to 80.00
     )
     //    val valorPedido = pedidos.getValue(5)
@@ -20,12 +20,11 @@ fun main() {
     println(pedidos.getOrDefault(0, -1.0))
 
     println(pedidos.keys)
-
     for(numero in pedidos.keys) {
         println("Pedido: $numero")
     }
-    println(pedidos.values)
 
+    println(pedidos.values)
     for (valor in pedidos.values) {
         println("Valor do pedido: $valor")
     }
@@ -33,20 +32,48 @@ fun main() {
     val pedidosFiltrados = pedidos.filter { (numero: Int,valor: Double) ->
         numero % 2 == 0 && valor > 50.0
     }
-    
     println(pedidosFiltrados)
 
     val pedidosAcima = pedidos.filterValues { valor ->
         valor > 70
     }
-
     println(pedidosAcima)
 
     val pedidosPares = pedidos.filterKeys { numero ->
         numero % 2 == 0
     }
-
     println(pedidosPares)
+
+    println(pedidos + mapOf(7 to 90.0, 8 to 20.0))
+    println(pedidos)
+
+    println(pedidos - listOf(6,5))
+    println(pedidos)
+
+//  pedidos.putAll(listOf<Pair<Int, Double>>(7 to 90.0, 8 to 20.0, 8 to 30.0))
+    pedidos += listOf<Pair<Int, Double>>(7 to 90.0, 8 to 20.0, 8 to 30.0)
+    println(pedidos)
+
+    pedidos.remove(6)
+    println(pedidos)
+
+    pedidos.remove(3, 100.0)
+    println(pedidos)
+
+    pedidos.remove(3, 50.0)
+    println(pedidos)
+
+    pedidos.keys.remove(1)
+    println(pedidos)
+
+    pedidos.values.remove(50.0)
+    println(pedidos)
+
+    pedidos.values.remove(100.0)
+    println(pedidos)
+
+    pedidos -= 6
+    println(pedidos)
 }
 
 
@@ -64,18 +91,14 @@ fun testaMap(pedidos: MutableMap<Int, Double>) {
     }
     pedidos[4] = 70.00
     println(pedidos)
+
     pedidos.put(5, 80.0)
     pedidos[1] = 100.0
     println(pedidos)
+
     pedidos.putIfAbsent(6, 200.0)
     println(pedidos)
-    pedidos.putIfAbsent(6, 150.0)
-    println(pedidos)
 
-    pedidos.remove(6)
-    println(pedidos)
-    pedidos.remove(3, 100.0)
-    println(pedidos)
-    pedidos.remove(3, 50.0)
+    pedidos.putIfAbsent(6, 150.0)
     println(pedidos)
 }
