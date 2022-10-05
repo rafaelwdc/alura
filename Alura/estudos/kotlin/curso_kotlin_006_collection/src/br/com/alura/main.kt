04615ad29ef834a0dd016ad8681e9c4d85559b70
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-05 16:20:24 -0300

    v1.a2.ck006

--- Alura/estudos/kotlin/curso_kotlin_006_collection/src/br/com/alura/main.kt
+++ Alura/estudos/kotlin/curso_kotlin_006_collection/src/br/com/alura/main.kt
@@ -1,37 +1,12 @@
 package br.com.alura
 
 fun main() {
-    val banco = BancoDeNomes()
-    val nomesSalvos: Collection<String> = banco.nomes
-    banco.salva("Rafael")
-    println(nomesSalvos)
-    println(BancoDeNomes().nomes)
+
+    val assistiramCursoAndroid = listOf("Rafael", "Patricky", "Camila", "Néia", "Dori")
+    val assistiramCursoKotlin = listOf("Steelx","Debler", "Dudinha", "Hiro")
+    val assistiramAmbos = assistiramCursoAndroid + assistiramCursoKotlin
+    println(assistiramAmbos.distinct())
+
+
 }
 
-class BancoDeNomes {
-    val nomes: Collection<String> get() = dados.toList()
-
-    fun salva(nome: String) {
-        dados.add(nome)
-    }
-
-    companion object {
-        private val dados = mutableListOf<String>()
-    }
-}
-
-fun testaColecao() {
-    val nomes: Collection<String> = mutableListOf(
-        "Rafael",
-        "Patricky",
-        "Camila",
-        "Mamãe",
-        "Veio Tanso"
-    )
-    for (nome in nomes) {
-        println(nome)
-    }
-    println(nomes)
-    println("Tem o nome Rafael? ${nomes.contains("Rafael")}")
-    println("Tamanho da Coleção ${nomes.size}")
-}
