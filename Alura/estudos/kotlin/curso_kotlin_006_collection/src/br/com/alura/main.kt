4643ec113774c4f8984ea125c7aea058e60c9551
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-06 17:18:28 -0300

    finalizada.ck006

--- Alura/estudos/kotlin/curso_kotlin_006_collection/src/br/com/alura/main.kt
+++ Alura/estudos/kotlin/curso_kotlin_006_collection/src/br/com/alura/main.kt
@@ -5,7 +5,8 @@ fun main() {
         Pedido(1, 20.0),
         Pedido(2, 60.0),
         Pedido(3, 30.0),
-        Pedido(4, 70.0)
+        Pedido(4, 70.0),
+        Pedido(4, 40.0)
     )
     println(pedidos)
     val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido: Pedido ->
@@ -18,6 +19,37 @@ fun main() {
         pedido.valor > 50.0
     }
     println(pedidosFreteGratis)
+    println(pedidosFreteGratis[Pedido(numero=2, valor=60.0)])
+
+//    val mapa: Map<Boolean, Pedido> = pedidos.associateBy { pedido ->
+//        pedido.valor > 50.0
+//    }
+//
+//    println(mapa)
+
+    val pedidosFreteGratisAgrupados = pedidos.groupBy { pedido: Pedido ->
+        pedido.valor > 50.0
+    }
+    println(pedidosFreteGratisAgrupados)
+    println(pedidosFreteGratisAgrupados[true])
+
+    val nomes = listOf(
+        "Rafael",
+        "Patricky",
+        "Camila",
+        "Néinha",
+        "Dori",
+        "Steela",
+        "Debler",
+        "Juinha",
+        "Hiro"
+    )
+    val agenda: Map<Char, List<String>>  = nomes.groupBy { nome ->
+        nome.first()
+    }
+    println(agenda)
+    println(agenda['D'])
+
 }
 
 data class Pedido ( val numero: Int, val valor: Double)
