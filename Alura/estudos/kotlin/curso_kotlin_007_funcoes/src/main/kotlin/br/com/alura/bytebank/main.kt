b2130b6fe03c60a16c217a42caaa6371bf2c1b58
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-06 17:52:08 -0300

    v2.a1.ck007

--- Alura/estudos/kotlin/curso_kotlin_007_funcoes/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ Alura/estudos/kotlin/curso_kotlin_007_funcoes/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,20 +1,6 @@
-import br.com.alura.bytebank.modelo.Endereco
-import java.lang.IllegalStateException
-
 fun main() {
-    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "Predio")
-    val logradouroNovo: String? = enderecoNulo?.logradouro
-    enderecoNulo?.let {
-        println(it.logradouro.length)
-        val tamanhoComplemento: Int =
-            it.complemento?.length ?: throw  IllegalStateException("Complemento não pode ser Vazio")
-        println(tamanhoComplemento)
-    }
-    teste(1)
-    teste("")
-}
 
-fun teste(valor: Any) {
-    val numero: Int? = valor as? Int
-    println(numero)
+    println(soma(1,5))
+    println(soma(1,5))
 }
+fun soma (a: Int, b: Int) : Int = a + b
---
+++ Alura/estudos/kotlin/curso_kotlin_007_funcoes/src/main/kotlin/br/com/alura/bytebank/teste/testaNullable.kt
@@ -0,0 +1,23 @@
+package br.com.alura.bytebank.teste
+
+import br.com.alura.bytebank.modelo.Endereco
+import java.lang.IllegalStateException
+
+
+fun testaNullable() {
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "Predio")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+        val tamanhoComplemento: Int =
+            it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser Vazio")
+        println(tamanhoComplemento)
+    }
+    teste(1)
+    teste("")
+}
+
+fun teste(valor: Any) {
+    val numero: Int? = valor as? Int
+    println(numero)
+}
