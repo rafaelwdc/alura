cf7c88dbb802a8afd222249f867d58f4d2c3731d
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-10 10:19:00 -0300

    v1.a3.ck007

--- Alura/estudos/kotlin/curso_kotlin_007_funcoes/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ Alura/estudos/kotlin/curso_kotlin_007_funcoes/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,49 +1,21 @@
+import br.com.alura.bytebank.modelo.Endereco
+
 fun main() {
-//    testaTipoFuncaoClasse()
-//    testaTipoFuncaoReferencia()
-
-    val minhaFuncaoLambda: (Int, Int) -> Int = { a: Int, b: Int ->
-        a + b
+    val testeFuncao: () -> Unit
+    Endereco().let {
+        it
     }
-    println(minhaFuncaoLambda(15,10))
+    "".let(::testeRecebeString)
+    1.let {
 
-    val minhaFuncaoAnonima: (Int, Int) -> Int = fun (a: Int, b: Int): Int {
-        return a + b
     }
-    println(minhaFuncaoAnonima(20, 10))
+    teste (1, {})
+}
 
-    val calculaBonificacao: (salario: Double) -> Double = lambda@  { salario ->
-        if(salario > 1000.0) {
-            return@lambda salario + 50
-        }
-        salario + 100.0
-    }
-    println(calculaBonificacao(1000.0))
-
-    val calculadoraBonificacaoAnonima: (salario: Double) -> Double = fun(salario) : Double {
-        if(salario > 1000.0) {
-            return salario + 50
-        }
-        return salario + 100
-    }
-    println(calculadoraBonificacaoAnonima(1100.0))
+fun testeRecebeString(valor: String) {
 
 }
 
-private fun testaTipoFuncaoClasse() {
-    val minhaFuncaoClasses: (Int, Int) -> Int = Soma()
-    println(minhaFuncaoClasses(10,10))
-}
+fun teste(teste: Int, bloco: () -> Unit) {
 
-private fun testaTipoFuncaoReferencia() {
-    val minhaFuncao: (Int, Int) -> Int = ::soma
-    println(minhaFuncao(5,10))
-}
-
-fun soma(a: Int, b: Int): Int {
-    return a + b
-}
-
-class Soma : (Int, Int) -> Int {
-    override fun invoke(a: Int, b: Int): Int = a + b
 }
