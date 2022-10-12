226443b1c765be2c594efff39164ce503e8ea967
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-12 12:24:35 -0300

    v1.a4.ack001

--- Alura/android/Orgs/app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ Alura/android/Orgs/app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -2,9 +2,7 @@ package br.com.alura.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
-import android.widget.Toast
 import br.com.alura.orgs.R
 
 class MainActivity : Activity() {
@@ -12,6 +10,12 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de Frutas"
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, Manga e Maçã"
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "19.99"
     }
 
 }
