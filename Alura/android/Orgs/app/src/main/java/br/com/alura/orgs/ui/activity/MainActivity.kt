5c26e5a178d7ac5c3878f9d8e5d8917dd8017d96
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-12 12:56:19 -0300

    v3.a4.ack001

--- Alura/android/Orgs/app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ Alura/android/Orgs/app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -2,19 +2,22 @@ package br.com.alura.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
+import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val nome = findViewById<TextView>(R.id.nome)
-        nome.text = "Cesta de Frutas"
-        val descricao = findViewById<TextView>(R.id.descricao)
-        descricao.text = "Laranja, Manga e Maçã"
-        val valor = findViewById<TextView>(R.id.valor)
-        valor.text = "19.99"
+//        val nome = findViewById<TextView>(R.id.nome)
+//        nome.text = "Cesta de Frutas"
+//        val descricao = findViewById<TextView>(R.id.descricao)
+//        descricao.text = "Laranja, Manga e Maçã"
+//        val valor = findViewById<TextView>(R.id.valor)
+//        valor.text = "19.99"
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter()
     }
 }
