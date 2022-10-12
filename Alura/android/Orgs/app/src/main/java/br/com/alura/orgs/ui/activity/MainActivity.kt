dbc2d48a30da1eeac7baeaf52cc21515daf6f76e
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-12 13:31:55 -0300

    finalizada.a4.ack001

--- Alura/android/Orgs/app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ Alura/android/Orgs/app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -2,9 +2,12 @@ package br.com.alura.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
+import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class MainActivity : Activity() {
 
@@ -18,6 +21,21 @@ class MainActivity : Activity() {
 //        val valor = findViewById<TextView>(R.id.valor)
 //        valor.text = "19.99"
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = ListaProdutosAdapter()
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
+            Produto(
+                nome = "Guaraná",
+                descricao = "Suco feito de guaraná amazonense colhido no coração da Amazonia",
+                valor = BigDecimal("9.99")
+            ), Produto(
+                nome = "Suco de caju",
+                descricao = "Suco feito de caju original do Piauí",
+                valor = BigDecimal("7.99")
+            ), Produto(
+                nome = "Goiabada",
+                descricao = "Colhidas no Sul de Minas as melhores goiabas do sudeste do Brasil",
+                valor = BigDecimal("12.99")
+            )
+        ))
+//        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
