fd77f55d0f87c46191dd2006e9a4acacfc3d61c9
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-13 19:54:30 -0300

    finalizada ack001

--- Alura/android/Orgs/app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
+++ Alura/android/Orgs/app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -27,7 +27,7 @@ class ListaProdutosActivity : AppCompatActivity(R.layout.activity_lista_produtos
     }
 
     private fun configuraFAB() {
-        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        val fab = findViewById<FloatingActionButton>(R.id.activity_lista_produto_fab)
         fab.setOnClickListener {
             vaiParaFormularioProduto()
         }
@@ -39,7 +39,7 @@ class ListaProdutosActivity : AppCompatActivity(R.layout.activity_lista_produtos
     }
 
     private fun configuraRecyclerView() {
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val recyclerView = findViewById<RecyclerView>(R.id.activity_lista_produto_recyclerView)
         recyclerView.adapter = adapter
     }
 }
