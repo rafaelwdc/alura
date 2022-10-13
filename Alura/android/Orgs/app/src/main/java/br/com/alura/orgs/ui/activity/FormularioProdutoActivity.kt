09199f2a2bec82b91187a2a3301d18ed7d2fb2c9
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-13 17:16:46 -0300

    finalizada a6.ack001

--- Alura/android/Orgs/app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
+++ Alura/android/Orgs/app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import br.com.alura.orgs.R
+import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity :
     AppCompatActivity(R.layout.activity_formulario_produto) {
@@ -19,7 +21,27 @@ class FormularioProdutoActivity :
         botaoSalvar.setOnClickListener {
             val campoNome = findViewById<EditText>(R.id.nome)
             val nome = campoNome.text.toString()
-            Log.i("FormularioProduto", "onCreate: $nome")        }
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val descricao = campoDescricao.text.toString()
+            val campoValor = findViewById<EditText>(R.id.valor)
+            val valorEmTexto = campoValor.text.toString()
+            val valor = if(valorEmTexto.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(valorEmTexto)
+            }
+
+            val produtoNovo = Produto(
+                nome = nome,
+                descricao = descricao,
+                valor = valor
+
+            )
+
+
+            Log.i("FormularioProduto", "onCreate: $produtoNovo")
+
+        }
 
     }
 
