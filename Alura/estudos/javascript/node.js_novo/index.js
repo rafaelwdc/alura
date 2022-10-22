e3c7c8db2d81c24490c50c17bfe793fbfd201799
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-10-22 13:16:29 -0300

    v1.a2.node.js

--- Alura/estudos/javascript/node.js_novo/index.js
+++ Alura/estudos/javascript/node.js_novo/index.js
@@ -1,19 +1,11 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('olá mundo'));
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+        console.log(chalk.green(texto));
+    });
+};
 
-console.log('olá mundo');
-
-//encadear métodos para colorir texto, cor de fundo e texto em negrito
-console.log(chalk.blue.bgWhite.bold('Alura'));
-//receber múltiplos argumentos
-console.log(chalk.blue('curso', 'de', 'Node.js'));
-//métodos aninhados
-console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
-// uso de template strings e placeholders
-console.log(`
-CPU: ${chalk.red('90%')}
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
-
+pegaArquivo('./arquivos/texto.md');
