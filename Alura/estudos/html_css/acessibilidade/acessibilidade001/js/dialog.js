faaf63130306204cb632b1d6099cbce29b303789
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-11-09 21:43:11 -0300

    v2.a2.acessibilidade003

--- Alura/estudos/html_css/acessibilidade/acessibilidade001/js/dialog.js
+++ Alura/estudos/html_css/acessibilidade/acessibilidade001/js/dialog.js
@@ -3,6 +3,7 @@ var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
 
 btnAbreDialog.style.display = 'block';
 
@@ -11,11 +12,13 @@ btnAbreDialog.style.display = 'block';
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
   document.querySelector('.dialogNewsletter-campo').focus();
+  conteudoForaDialog.inert = true;
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
   dialog.classList.remove('dialogNewsletter--aberto');     
+  conteudoForaDialog.inert = false;
 }
 
 // Listeners
