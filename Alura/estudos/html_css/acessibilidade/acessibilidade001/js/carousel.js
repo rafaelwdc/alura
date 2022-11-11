6ca9c1895428e801b18bff26d6c056aee12e07ed
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-11-10 23:34:08 -0300

    v.a3.acessibilidade003

--- Alura/estudos/html_css/acessibilidade/acessibilidade001/js/carousel.js
+++ Alura/estudos/html_css/acessibilidade/acessibilidade001/js/carousel.js
@@ -1,15 +1,16 @@
 // Variaveis
 
 var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+var noticias = document.querySelectorAll('.listaDeArtigos-item');
 var new0 = document.querySelector('#new0');
 var new1 = document.querySelector('#new1');
 var new2 = document.querySelector('#new2');
 
 document.querySelector('#new0').style.display = 'block';
 
-new0.style.display = "block"
+new0.style.display = "block";
 
-// Criando indicador de slide Atual
+// Criando indicador de slide Atual;
 
 var indicadorSlideAtual = document.createElement('span');
 indicadorSlideAtual.classList.add('escondeVisualmente');
@@ -19,33 +20,78 @@ indicadorSlideAtual.textContent = '(Slide Atual)';
 
 
 
-// Percorre todos os botoes controladores
+// Percorre todos os botoes controladores;
 btns.forEach(function(btn) {
   btn.addEventListener('click', function() {
 
-    if(this.getAttribute('data-sliderItem') === '0') {
-      new0.style.display = "block"
-      new1.style.display = "none"
-      new2.style.display = "none"
-    } else if (this.getAttribute('data-sliderItem') === '1'){
-      new0.style.display = "none"
-      new1.style.display = "block"
-      new2.style.display = "none"
-    } else {
-      new0.style.display = "none"
-      new1.style.display = "none"
-      new2.style.display = "block"
-    }
+    // btn.href = 'javascript:void(0)';
 
+    noticias.forEach(function(noticia) {
+      noticia.style.display = 'none';
+      if(this.getAttribute('data-sliderItem') === noticia.getAttribute('data-noticia')) {
+        noticia.style.display = 'block';
+      };
+    });
     
     document.querySelector('.listaDeArtigos-slider-item .escondeVisualmente').remove();
     this.append(indicadorSlideAtual);
 
-    // Remove classe 'ativo' dos outros botoes
+    // Remove classe 'ativo' dos outros botoes;
     btns.forEach(function(btnRemoveClass) {
-      btnRemoveClass.classList.remove('listaDeArtigos-slider-item--ativo')
-    })
+      btnRemoveClass.classList.remove('listaDeArtigos-slider-item--ativo');
+    });
 
-    this.classList.add('listaDeArtigos-slider-item--ativo')
-  })
-})
+    this.classList.add('listaDeArtigos-slider-item--ativo');
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // if(this.getAttribute('data-sliderItem') === '0') {
+    //   new0.style.display = "block"
+    //   new1.style.display = "none"
+    //   new2.style.display = "none"
+    // }
+    // if(this.getAttribute('data-sliderItem') === '0') {
+    //   new0.style.display = "block"
+    //   new1.style.display = "none"
+    //   new2.style.display = "none"
+    // } else if (this.getAttribute('data-sliderItem') === '1'){
+    //   new0.style.display = "none"
+    //   new1.style.display = "block"
+    //   new2.style.display = "none"
+    // } else {
+    //   new0.style.display = "none"
+    //   new1.style.display = "none"
+    //   new2.style.display = "block"
+    // }
