af9a17fa0047015d5841450b26ee9a084848a7c3
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-04 02:14:06 -0300

    cria o arquivo app.js v2.a2.validação-formulario/front-end

--- Alura/estudos/html_css/alura-petshop/validacao-doguito-main/js/validacao.js
+++ Alura/estudos/html_css/alura-petshop/validacao-doguito-main/js/validacao.js
@@ -1,23 +1,48 @@
-const dataNascimento = document.querySelector('#nascimento');
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
 
-dataNascimento.addEventListener('blur', (evento) => {
-    validaDataNascimento(evento.target);
-});
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    };
+};
+
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+};
+
 
 function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value);
     let mensagem = '';
-
+    
     if(!maiorQue18(dataRecebida)) {
         mensagem = 'Você deve ser maior de 18 anos para se cadastrar.';
     };
-
+    
     input.setCustomValidity(mensagem);
 };
 
 function maiorQue18(data) {
     const dataAtual = new Date();
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
-
+    
     return dataMais18 <= dataAtual;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const dataNascimento = document.querySelector('#nascimento');
+
+// dataNascimento.addEventListener('blur', (evento) => {
+//     validaDataNascimento(evento.target);
+// });
