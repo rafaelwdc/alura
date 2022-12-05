5d15aced8c4ce185f882203240dfd7dd8c9dbe3f
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-05 20:14:43 -0300

    aprendendo sobre os eventos v2.a2.robotron-2000/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/robotron-2000/js/main.js
+++ Alura/estudos/javascript/javascript-front-end/robotron-2000/js/main.js
@@ -1,12 +1,12 @@
 const robotron = document.querySelector('#robotron');
 
-robotron.addEventListener('click', () => {
-    console.log('Cliquei no robo!');
+robotron.addEventListener('click', (evento) => {
+    console.log(evento);
 });
 
-function dizOi() {
-    console.log('oi!');
+function dizOi(nome) {
+    console.log(`Olá, ${nome}.`);
     console.log('Bem-vindo ao Robotron 2000');
 };
 
-dizOi();
+dizOi('Rafa');
