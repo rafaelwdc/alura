c594cf1f47cca33dee2d5e139e5d322af6a6b8e4
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-06 02:51:23 -0300

    integrando o localStorage no html v1.a4 muchila-de-viagem/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/mochila-de-viagem/js/main.js
+++ Alura/estudos/javascript/javascript-front-end/mochila-de-viagem/js/main.js
@@ -12,14 +12,23 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
 
+    const existe = itens.find(elemento => elemento.nome === nome.value);
+    
     const itemAtual = {
-    "nome": nome.value,
-    "quantidade": quantidade.value
+        "nome": nome.value,
+        "quantidade": quantidade.value
     };
 
-    criaElemento(itemAtual);
+    if(existe) {
+        itemAtual.id = existe.id;
+        atualizaElemento(itemAtual);
+    } else {
+        itemAtual.id = itens.length;
+        criaElemento(itemAtual);
+    
+        itens.push(itemAtual);
+    }
 
-    itens.push(itemAtual);
 
     localStorage.setItem("itens", JSON.stringify(itens));
 
@@ -33,9 +42,14 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement('strong');
     numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem);
 
     novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
+};
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
 };
