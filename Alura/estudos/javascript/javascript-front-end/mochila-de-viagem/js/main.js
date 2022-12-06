c8b9c5d9fb303529a73d4276edfe300661bbb2c7
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-06 02:09:49 -0300

    inicio da refatoração v1.a3 muchila-de-viagem/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/mochila-de-viagem/js/main.js
+++ Alura/estudos/javascript/javascript-front-end/mochila-de-viagem/js/main.js
@@ -1,16 +1,15 @@
-
-// Operador lógico que retorna com dados salvos, ou string vazia, utilizando localStorage.getItem, modificando o valor de `string` com JSON.parse()
-
 const form = document.getElementById("novoItem") 
 const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []   
+const itens = JSON.parse(localStorage.getItem("itens")) || []  
 
-// Uso do forEach para que todos os itens já escritos na lista sejam mantidos ao atualizar a página 
+itens.forEach((elemento) => {
+    console.log(elemento.nome, elemento.quantidade);
+});
+
 // itens.forEach( (elemento) => {    
 //     criaElemento(elemento)
 // } )     
 
-// Refatoração do addEventListener para receber as funções extras da função criaElemento
 form.addEventListener("submit", (evento) => {   
     evento.preventDefault()            
 
@@ -32,8 +31,6 @@ form.addEventListener("submit", (evento) => {
     quantidade.value = ""
 })
 
-// Refatoração da função `criaElemento` para que possua apenas a função que faça sentido ao nome. 
-
 function criaElemento(item) {  
     const novoItem = document.createElement('li')
     novoItem.classList.add("item")
@@ -46,51 +43,3 @@ function criaElemento(item) {
 
     lista.appendChild(novoItem)
 }
-
-
-// const form = document.getElementById('novoItem');
-// const lista = document.getElementById('lista');
-// const itens = JSON.parse(localStorage.getItem("itens")) || [];
-
-// console.log(itens);
-// console.log([]);
-
-// // itens.forEach( (elemento) => {
-// //     criaElemento(elemento);
-// // });
-
-// form.addEventListener("submit", (evento) => {
-//     evento.preventDefault();
-    
-//     const nome = evento.target.elements['nome'];
-//     const quantidade = evento.target.elements['quantidade'];
-
-//     const itemAtual = {
-//         "nome": nome.value,
-//         "quantidade": quantidade.value
-//     };
-
-//     criaElemento(itemAtual);
-
-//     itens.push(itemAtual);
-
-//     localStorage.setItem("itens", JSON.stringify(itens));
-
-//     nome.value = "";
-//     quantidade.value = "";
-// });
-
-// function criaElemento(item) {
-
-//     const novoItem = document.createElement("li");
-//     novoItem.classList.add("item");
-
-//     const numeroItem = document.createElement("strong");
-//     numeroItem.innerHTML = quantidade;
-//     novoItem.appendChild(numeroItem);
-
-//     novoItem.innerHTML += item.nome;
-
-//     lista.appendChild(novoItem)
-
-// };
