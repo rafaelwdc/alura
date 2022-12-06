3552dabfef2eafa5cac39f301dfbe304bdbd00bf
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-06 01:02:55 -0300

    criando a função itemAtual v1.a3 muchila-de-viagem/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/mochila-de-viagem/js/main.js
+++ Alura/estudos/javascript/javascript-front-end/mochila-de-viagem/js/main.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
+const itens = [];
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
@@ -29,6 +30,12 @@ function criaElemento(nome, quantidade) {
 
     lista.appendChild(novoItem)
 
-    localStorage.setItem('nome', nome);
-    localStorage.setItem('quantidade', quantidade);
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    };
+
+    itens.push(itemAtual);
+    localStorage.setItem('item', JSON.stringify(itens));
+    
 };
