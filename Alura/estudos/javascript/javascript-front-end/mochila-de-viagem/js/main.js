f1d4afa49799ee8e66580815e6f2635e68c8c40c
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-06 02:36:34 -0300

    update localStorage acessado pelo arquivo v2.a3 muchila-de-viagem/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/mochila-de-viagem/js/main.js
+++ Alura/estudos/javascript/javascript-front-end/mochila-de-viagem/js/main.js
@@ -1,92 +1,41 @@
-const form = document.getElementById("novoItem") 
-const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []  
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
 itens.forEach( (elemento) => {    
-    criaElemento(elemento)
-} )
+    criaElemento(elemento);
+} );
 
 form.addEventListener("submit", (evento) => {  
-    evento.preventDefault()
+    evento.preventDefault();
 
-    const nome = evento.target.elements['nome']
-    const quantidade = evento.target.elements['quantidade']
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
 
     const itemAtual = {
     "nome": nome.value,
     "quantidade": quantidade.value
-    }
+    };
 
-    criaElemento(itemAtual)
+    criaElemento(itemAtual);
 
-    itens.push(itemAtual)
+    itens.push(itemAtual);
 
-    localStorage.setItem("itens", JSON.stringify(itens))
+    localStorage.setItem("itens", JSON.stringify(itens));
 
-    nome.value = ""
-    quantidade.value = ""
-})
+    nome.value = "";
+    quantidade.value = "";
+});
 
 function criaElemento(item) {  
-    const novoItem = document.createElement('li')
-    novoItem.classList.add("item")
+    const novoItem = document.createElement('li');
+    novoItem.classList.add("item");
 
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = item.quantidade
-    novoItem.appendChild(numeroItem)
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = item.quantidade;
+    novoItem.appendChild(numeroItem);
 
-    novoItem.innerHTML += item.nome
+    novoItem.innerHTML += item.nome;
 
-    lista.appendChild(novoItem)
-}
-
-
-
-
-
-
-
-
-
-
-// const form = document.getElementById("novoItem");
-// const lista = document.getElementById("lista");
-// const itens = JSON.parse(localStorage.getItem("itens")) || [];
-
-// itens.forEach((elemento) => {
-//     criaElemento(elemento);
-// });
-
-// form.addEventListener("submit", (evento) => {   
-//     evento.preventDefault();           
-
-//     const nome = evento.target.elements['nome'];
-//     const quantidade = evento.target.elements['quantidade'];
-
-//     const itemAtual = {
-//     "nome": nome.value,
-//     "quantidade": quantidade.value
-//     };
-
-//     criaElemento(itemAtual);
-
-//     itens.push(itemAtual);
-
-//     localStorage.setItem("itens", JSON.stringify(itens));
-
-//     nome.value = "";
-//     quantidade.value = "";
-// });
-
-// function criaElemento(item) {  
-//     const novoItem = document.createElement('li');
-//     novoItem.classList.add("item");
-
-//     const numeroItem = document.createElement('strong');
-//     numeroItem.innerHTML = item.quantidade;
-//     novoItem.appendChild(numeroItem);
-
-//     novoItem.innerHTML += item.nome;
-
-//     lista.appendChild(novoItem);
-// };
+    lista.appendChild(novoItem);
+};
