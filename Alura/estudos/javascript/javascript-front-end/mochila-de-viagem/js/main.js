7b51091d2d9be00b6a324c67a15ac01043fe5952
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-05 22:45:40 -0300

    adicionando os items via js v3.a1 muchila-de-viagem/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/mochila-de-viagem/js/main.js
+++ Alura/estudos/javascript/javascript-front-end/mochila-de-viagem/js/main.js
@@ -1,8 +1,25 @@
 const form = document.getElementById('novoItem');
+const lista = document.getElementById('lista');
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
-    console.log(evento);
-    console.log(evento.target.elements['nome'].value);
-    console.log(evento.target.elements['quantidade'].value);
-})
+
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
+});
+
+function criaElemento(nome, quantidade) {
+    console.log(nome);
+    console.log(quantidade);
+
+    let novoItem = document.createElement('li');
+    novoItem.classList.add("item");
+
+    let numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
+    
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem)
+
+};
