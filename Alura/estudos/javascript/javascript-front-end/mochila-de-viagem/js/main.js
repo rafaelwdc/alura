f780f9e508d05001b71e59ce946f3cf0dd9fabb1
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-06 02:34:50 -0300

    localStorage acessado pelo arquivo v2.a3 muchila-de-viagem/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/mochila-de-viagem/js/main.js
+++ Alura/estudos/javascript/javascript-front-end/mochila-de-viagem/js/main.js
@@ -2,16 +2,12 @@ const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
 const itens = JSON.parse(localStorage.getItem("itens")) || []  
 
-itens.forEach((elemento) => {
-    console.log(elemento.nome, elemento.quantidade);
-});
+itens.forEach( (elemento) => {    
+    criaElemento(elemento)
+} )
 
-// itens.forEach( (elemento) => {    
-//     criaElemento(elemento)
-// } )     
-
-form.addEventListener("submit", (evento) => {   
-    evento.preventDefault()            
+form.addEventListener("submit", (evento) => {  
+    evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
@@ -43,3 +39,54 @@ function criaElemento(item) {
 
     lista.appendChild(novoItem)
 }
+
+
+
+
+
+
+
+
+
+
+// const form = document.getElementById("novoItem");
+// const lista = document.getElementById("lista");
+// const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+// itens.forEach((elemento) => {
+//     criaElemento(elemento);
+// });
+
+// form.addEventListener("submit", (evento) => {   
+//     evento.preventDefault();           
+
+//     const nome = evento.target.elements['nome'];
+//     const quantidade = evento.target.elements['quantidade'];
+
+//     const itemAtual = {
+//     "nome": nome.value,
+//     "quantidade": quantidade.value
+//     };
+
+//     criaElemento(itemAtual);
+
+//     itens.push(itemAtual);
+
+//     localStorage.setItem("itens", JSON.stringify(itens));
+
+//     nome.value = "";
+//     quantidade.value = "";
+// });
+
+// function criaElemento(item) {  
+//     const novoItem = document.createElement('li');
+//     novoItem.classList.add("item");
+
+//     const numeroItem = document.createElement('strong');
+//     numeroItem.innerHTML = item.quantidade;
+//     novoItem.appendChild(numeroItem);
+
+//     novoItem.innerHTML += item.nome;
+
+//     lista.appendChild(novoItem);
+// };
