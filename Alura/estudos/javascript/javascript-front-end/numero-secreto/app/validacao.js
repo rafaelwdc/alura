f905c542fb8133ff2c45178d2d20d21e67fccf20
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-07 23:17:45 -0300

    colocando as dicas v3.a4.numero-secreto/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/numero-secreto/app/validacao.js
+++ Alura/estudos/javascript/javascript-front-end/numero-secreto/app/validacao.js
@@ -2,17 +2,27 @@ function verificaSeOChuteTemOValorValido(chute) {
     const numero = +chute;
     if(chuteForInvalido(numero)) {
         elementoChute.innerHTML += `<div>Valor inválido</div>`;
+        return;
     };
     if (numeroForMaiorouMenorQueOValorPermitido(numero)) {
         elementoChute.innerHTML += `
             <div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>
         `;
+        return;
     };
 
     if(numero === numeroSecreto) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>Seu numero secreto era: ${numeroSecreto}</h3>
+        `;
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML = `
+            <div>O número secreto é menor! <i class="fa-solid fa-down-long"></i></div>
+        `;
+    } else {
+        elementoChute.innerHTML = `
+            <div>O número secreto é maior! <i class="fa-solid fa-up-long"></i></div>
         `;
     }
 };
