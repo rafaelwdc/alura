30ea42de1bfe692138f2640e57aba12522b2f486
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-07 21:47:04 -0300

    ativando o reconhecimento por voz v2.a3.numero-secreto/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/numero-secreto/app/reconhecimentoDeVoz.js
+++ Alura/estudos/javascript/javascript-front-end/numero-secreto/app/reconhecimentoDeVoz.js
@@ -3,3 +3,10 @@ window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 recognition.lang = "pt-BR";
 recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(e) {
+    console.log(e.results[0][0].transcript);
+    
+}
