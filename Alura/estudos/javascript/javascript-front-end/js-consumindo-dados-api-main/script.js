692952a3d9a09fbe716939c6dee9d0f12567e1ec
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-09 20:01:21 -0300

    adicionando o finally() v3.a3.alurabooks-form/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/js-consumindo-dados-api-main/script.js
+++ Alura/estudos/javascript/javascript-front-end/js-consumindo-dados-api-main/script.js
@@ -1,10 +1,11 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/')
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
 .then(resposta => resposta.json())
 .then(r => {
     if(r.erro) {
         throw Error('Esse CEP não existe');
     } else
     console.log(r)})
-.catch(erro => console.log(erro));
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento concluído!'));
 
 console.log(consultaCEP);
