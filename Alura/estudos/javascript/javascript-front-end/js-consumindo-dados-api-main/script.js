c2625f2449e0c8b5015a3d8c6ad12067ead20ad4
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-09 20:17:18 -0300

    conhecendo a try() e catch() v1.a4.alurabooks-form/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/js-consumindo-dados-api-main/script.js
+++ Alura/estudos/javascript/javascript-front-end/js-consumindo-dados-api-main/script.js
@@ -1,7 +1,14 @@
 async function buscaEndereco() {
-    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
-    var consultaCEPConvertida = await consultaCEP.json();
-    console.log(consultaCEPConvertida);
+    try {
+        var consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/');
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        };
+        console.log(consultaCEPConvertida);
+    } catch(erro) {
+        console.log(erro);
+    }
 };
 
 buscaEndereco();
