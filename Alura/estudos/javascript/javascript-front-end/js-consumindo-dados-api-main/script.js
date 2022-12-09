68e11917e5dd79f188d23e2d1085c4fe26849b97
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-09 20:24:51 -0300

    adicionando o array com varios ceps v2.a4.alurabooks-form/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/js-consumindo-dados-api-main/script.js
+++ Alura/estudos/javascript/javascript-front-end/js-consumindo-dados-api-main/script.js
@@ -1,14 +1,19 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try {
-        var consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/');
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
             throw Error('CEP não existente!');
         };
         console.log(consultaCEPConvertida);
+        return consultaCEPConvertida
     } catch(erro) {
         console.log(erro);
     }
 };
 
-buscaEndereco();
+let ceps = ['01001000', '01001001'];
+let conjuntoCEPs = ceps = ceps.map(valores => buscaEndereco(valores));
+console.log(conjuntoCEPs);
+Promise.all(conjuntoCEPs)
+.then(respostas => console.log(respostas));
