94c7fb30515fb74544b230938c0ca5f0eceec92d
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-09 20:09:19 -0300

    conhecendo a async function e await v1.a4.alurabooks-form/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/js-consumindo-dados-api-main/script.js
+++ Alura/estudos/javascript/javascript-front-end/js-consumindo-dados-api-main/script.js
@@ -1,11 +1,7 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if(r.erro) {
-        throw Error('Esse CEP não existe');
-    } else
-    console.log(r)})
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('Processamento concluído!'));
+async function buscaEndereco() {
+    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
+    var consultaCEPConvertida = await consultaCEP.json();
+    console.log(consultaCEPConvertida);
+};
 
-console.log(consultaCEP);
+buscaEndereco();
