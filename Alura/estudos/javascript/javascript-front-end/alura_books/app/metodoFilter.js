bf22244db295d049417150e3040e86d2b8444b3a
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-10 02:42:11 -0300

    finalizando o reduce() v3.a5.alura-books/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/alura_books/app/metodoFilter.js
+++ Alura/estudos/javascript/javascript-front-end/alura_books/app/metodoFilter.js
@@ -7,7 +7,8 @@ function filtrarLivros() {
     let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria);
     exibirOsLivrosNaTela(livrosFiltrados);
     if(categoria == 'disponivel') {
-        exibirValorTotalDosLivrosDisponiveisNaTela();
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados);
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal);
     };
 };
 
@@ -19,10 +20,10 @@ function filtrarPorDisponibilidade() {
     return livros.filter(livro => livro.quantidade > 0);
 }
 
-function exibirValorTotalDosLivrosDisponiveisNaTela() {
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
     elementoComOValorTotalDeLivrosDisponiveis.innerHTML = `
     <div class="livros__disponiveis">
-    <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
     </div>
     `;
 };
