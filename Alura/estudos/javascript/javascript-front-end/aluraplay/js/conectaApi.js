ab1744455e583e000523846f2aa3bdf06e1d1a09
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-11 02:25:00 -0300

    adicionado o conectaApi() v1.a2.aluraplay/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/aluraplay/js/conectaApi.js
+++ Alura/estudos/javascript/javascript-front-end/aluraplay/js/conectaApi.js
@@ -1,6 +1,10 @@
 async function listaVideos() {
     const conexao = await fetch('http://localhost:3000/videos');
-    console.log(conexao);
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
 };
 
-listaVideos();
+export const conectaApi = {
+    listaVideos
+};
