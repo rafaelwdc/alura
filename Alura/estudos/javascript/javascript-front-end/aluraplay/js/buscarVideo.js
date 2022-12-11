97d7cff7b3e2f6d5095e164f95f8ee89ddae9539
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-11 20:35:20 -0300

    linkando os arquivos de busca a3.a4.aluraplay/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/aluraplay/js/buscarVideo.js
+++ Alura/estudos/javascript/javascript-front-end/aluraplay/js/buscarVideo.js
@@ -1,4 +1,5 @@
 import { conectaApi } from "./conectaApi.js";
+import { constroiCard } from "./mostrarVideos.js";
 
 async function buscarVideo(evento) {
     evento.preventDefault();
@@ -6,7 +7,16 @@ async function buscarVideo(evento) {
     const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
     const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 
-    console.log(busca);
+    const lista = document.querySelector("[data-lista]");
+
+    busca.forEach(elemento => lista.appendChild(
+        constroiCard(
+            elemento.titulo, 
+            elemento.descricao, 
+            elemento.url, 
+            elemento.imagem
+        )
+    ));
 };
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
