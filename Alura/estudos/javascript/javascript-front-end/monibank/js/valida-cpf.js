9ce7a08ea7a3586f741a8c96d4671450272a78fb
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-12 01:39:48 -0300

    validando primeiro digito do cpf v1.a3.monibank/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/monibank/js/valida-cpf.js
+++ Alura/estudos/javascript/javascript-front-end/monibank/js/valida-cpf.js
@@ -1,10 +1,9 @@
 export default function ehUmCPF (campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
     validaNumerosRepetidos(cpf);
-
+    validaPrimeiroDigito(cpf);
     console.log(validaNumerosRepetidos(cpf));
 };
-
 function validaNumerosRepetidos(cpf) {
     const numerosRepetidos = [
         '00000000000',
@@ -18,6 +17,20 @@ function validaNumerosRepetidos(cpf) {
         '88888888888',
         '99999999999'
     ];
-
     return numerosRepetidos.includes(cpf);
+};
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+    for(let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    };
+    soma = (soma * 10) % 11;
+
+    if(soma == 10 || soma == 11) {
+        soma = 0;
+    };
+    return soma != cpf[9];
 };
