ea6c966078d08922de1e3222b4ce98853deb7cfd
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-12 01:58:18 -0300

    validando data de nascimento v3.a3.monibank/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/monibank/js/script.js
+++ Alura/estudos/javascript/javascript-front-end/monibank/js/script.js
@@ -1,4 +1,5 @@
 import ehUmCPF from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js";
 const camposDoFormulario = document.querySelectorAll("[required]");
 
 camposDoFormulario.forEach((campo) => {
@@ -6,7 +7,10 @@ camposDoFormulario.forEach((campo) => {
 });
 
 function verificaCampo(campo) {
-    if(campo.name == 'cpf' && campo.value.length >= 11) {
+    if (campo.name == 'cpf' && campo.value.length >= 11) {
         ehUmCPF(campo);
+    };
+    if (campo.name == "aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo);
     };
 };
---
+++ Alura/estudos/javascript/javascript-front-end/monibank/js/valida-idade.js
@@ -0,0 +1,14 @@
+export default function ehMaiorDeIdade(campo) {
+    const dataNascimento = new Date(campo.value);
+    validaIdade(dataNascimento);
+
+    console.log(validaIdade(dataNascimento));
+};
+
+function validaIdade(data) {
+    const dataAtual = new Date();
+    const dataMaiorDeIdade = new Date(
+        data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+        return dataAtual >= dataMaiorDeIdade;
+}
