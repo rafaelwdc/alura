4e9efe69b9179633fa6397e7f28e5c02a7546d60
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-11 21:08:30 -0300

    adicionando alerta de erro a2.a5.aluraplay/javascript-front-end

--- Alura/estudos/javascript/javascript-front-end/aluraplay/js/conectaApi.js
+++ Alura/estudos/javascript/javascript-front-end/aluraplay/js/conectaApi.js
@@ -1,5 +1,5 @@
 async function listaVideos() {
-    const conexao = await fetch('http://localhost:3000/video');
+    const conexao = await fetch('http://localhost:3000/videos');
     const conexaoConvertida = await conexao.json();
 
     return conexaoConvertida;
@@ -18,8 +18,11 @@ async function criaVideo(titulo, descricao, url, imagem) {
             imagem: imagem
         })
     });
-
+    if (!conexao.ok) {
+        throw new Error('Não foi possível enviar o vídeo!');
+    };
     const conexaoConvertida = await conexao.json();
+
     return conexaoConvertida;
 };
 
