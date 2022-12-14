4c8b4aa56cf4d29c0381ef192b64d46aa2c90f2b
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-13 23:31:28 -0300

    procurando dentro de uma lista v1.a3.arrays/javascript-back-end

---
+++ Alura/estudos/javascript/javascript-back-end/procurando-aluno.js
@@ -0,0 +1,20 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+function exibeNomeENota(aluno) {
+    if (listaDeAlunosEMedias[0].includes(aluno)) {
+        console.log(`${aluno} está cadastrado!`);
+
+        const indice = listaDeAlunosEMedias[0].indexOf(aluno);
+
+        const mediaDoAluno = listaDeAlunosEMedias[1][indice];
+
+        console.log(`${aluno} tem a média ${mediaDoAluno}`);
+    } else {
+        console.log(`Aluno não cadastrado!`);
+    };
+};
+
+exibeNomeENota("Ana");
