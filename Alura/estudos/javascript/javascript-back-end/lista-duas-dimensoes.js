c827426a990d2f4606df0f08cdbeea9aad83641c
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-13 23:20:16 -0300

    aninhando arrays dentro de arrays v4.a2.arrays/javascript-back-end

---
+++ Alura/estudos/javascript/javascript-back-end/lista-duas-dimensoes.js
@@ -0,0 +1,10 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log(`
+    O(A) primeiro(a) aluno(a) da lista de alunos é: ${listaDeAlunosEMedias[0][1]}.
+    A nota dessa aluno(a) é ${listaDeAlunosEMedias[1][1]};
+    `
+);
