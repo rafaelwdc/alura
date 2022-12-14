6b7f3c6cb85dabab5f9c20128e04a0da65a61d4e
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-13 22:45:00 -0300

    dividindo um array com slice(); v1.a2.arrays/javascript-back-end

---
+++ Alura/estudos/javascript/javascript-back-end/dividindo-sala.js
@@ -0,0 +1,30 @@
+const alunos = [
+    "João",
+    "Juliana",
+    "Ana",
+    "Caio",
+    "Lara",
+    "Marjorie",
+    "Guilherme",
+    "Aline",
+    "Fabiana",
+    "André",
+    "Carlos",
+    "Paulo",
+    "Bia",
+    "Vivian",
+    "Isabela",
+    "Vinicius",
+    "Renan",
+    "Renata",
+    "Dayse",
+    "Camilo"
+ ];
+
+ const sala1 = alunos.slice(0,alunos.length / 2);
+ const sala2 = alunos.slice(alunos.length / 2);
+
+ console.log(`\nAlunos da sala 1: ${sala1}`);
+ console.log(`\nAlunos da sala 2: ${sala2}\n`);
+
+
