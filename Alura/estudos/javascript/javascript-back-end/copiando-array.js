1c7970c72d5e4378a6ded0951153f04cfd3c15f5
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-14 02:28:38 -0300

    conhecendo o spread operator v3.a5.arrays/javascript-back-end

---
+++ Alura/estudos/javascript/javascript-back-end/copiando-array.js
@@ -0,0 +1,8 @@
+const notas = [7, 7, 8, 9];
+
+const novaNotas = [...notas, 10];
+
+// novaNotas.push(10);
+
+console.log(`As novas notas são ${novaNotas}`);
+console.log(`As novas originais são ${notas}`);
