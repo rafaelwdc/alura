1e1dbf227a0d2fe890f39585c7dae54817891122
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-14 03:06:27 -0300

    iniciando com objetos v1.a1.objetos/javascript-back-end

---
+++ Alura/estudos/javascript/javascript-back-end/objetos/aula1/objetos.js
@@ -0,0 +1,9 @@
+const listaCPFs = [11111111111,22222222222,33333333333];
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "11122233344"];
+
+console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+    nome: "José",
+    idade: 32
+}
