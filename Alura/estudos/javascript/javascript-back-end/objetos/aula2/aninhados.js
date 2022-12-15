7eecc7761bf08d640d62f3fd94e7ad063d158fff
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-15 20:12:16 -0300

    criando arquivo aninhamentos.js v2.a2.objetos/javascript-back-end

---
+++ Alura/estudos/javascript/javascript-back-end/objetos/aula2/aninhados.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "Rafael",
+    idade: 29,
+    email: "rafael@eu.com",
+    telefone: ["11 555 555 550","11 444 444 440"]
+};
+
+cliente.endereco = {
+    rua: "Rua Joseph Climber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "Ap 934"
+};
+console.log(cliente.endereco);
