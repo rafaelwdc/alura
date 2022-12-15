1493932f66abb2e724b07428161df2f653307614
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-14 23:05:35 -0300

    criando arquivo alterar.js v4.a1.objetos/javascript-back-end

---
+++ Alura/estudos/javascript/javascript-back-end/objetos/aula1/alterar.js
@@ -0,0 +1,16 @@
+const pessoa = {
+    nome: "Luma",
+    profissao: "Engenheira"
+};
+
+console.log(pessoa.nome);
+
+console.log(pessoa.telefone);
+
+pessoa.telefone = "11 222 333 444";
+
+console.log(pessoa.telefone);
+
+pessoa.nome = "Luma Silva";
+
+console.log(pessoa);
