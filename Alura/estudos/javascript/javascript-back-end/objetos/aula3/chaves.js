3ff15ddd1b100f581eea61601e2898008812c6cd
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-15 21:03:10 -0300

    criando arquivo chaves.js v2.a3.objetos/javascript-back-end

---
+++ Alura/estudos/javascript/javascript-back-end/objetos/aula3/chaves.js
@@ -0,0 +1,22 @@
+const cliente = {
+    nome: "Rafael",
+    idade: 29,
+    email: "rafael@eu.com",
+    telefone: ["11 555 555 550","11 444 444 440"]
+};
+
+// cliente.enderecos = [{
+//     rua: "Rua Joseph Climber",
+//     numero: 1337,
+//     apartamento: true,
+//     complemento: "Ap 934"
+//     }
+// ];
+
+const chavesDoObjeto = Object.keys(cliente);
+
+console.log(chavesDoObjeto);
+
+if (!chavesDoObjeto.includes("enderecos")) {
+    console.error(`É necessario ter um endereço cadastrado!`)
+}
