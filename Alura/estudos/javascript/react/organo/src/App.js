6c0317c0772bff66eae3811409209331c1d6159d
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-17 19:55:14 -0300

    criando o componente CampoTexto v5.a1.react:desenvolvendo-com-javascript/react-javascript

--- Alura/estudos/javascript/react/organo/src/App.js
+++ Alura/estudos/javascript/react/organo/src/App.js
@@ -1,10 +1,11 @@
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto/CampoTexto';
 
 function App () {
   return (
     <div className="App">
       <Banner/>
-      
+      <CampoTexto />
     </div>
   );
 }
