185b5c7882dc49c8c5ffecd9a35725b179db205b
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-18 00:37:09 -0300

    controlando os estados v2.a3.react:desenvolvendo-com-javascript/react-javascript

--- Alura/estudos/javascript/react/organo/src/componentes/CampoTexto/index.js
+++ Alura/estudos/javascript/react/organo/src/componentes/CampoTexto/index.js
@@ -4,11 +4,8 @@ const CampoTexto = (props) => {
 
     const placeholderModificado = `${props.placeholder}...`
 
-    let valor = 'Rafael'
-
     const aoDigitado = (evento) => {
-        valor = evento.target.value
-        console.log(valor);
+        props.aoAlterado(evento.target.value)
     }
 
     return (
@@ -16,7 +13,7 @@ const CampoTexto = (props) => {
             <label>
                 {props.label}
             </label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificado} />
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificado} />
         </div>
     )
 }
