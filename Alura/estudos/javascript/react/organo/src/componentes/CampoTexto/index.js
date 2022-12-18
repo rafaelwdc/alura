aab4bcd3da53919ab8ccc082b64d8983428c0614
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-17 23:40:20 -0300

    manipulando o input do usuario v1.a3.react:desenvolvendo-com-javascript/react-javascript

--- Alura/estudos/javascript/react/organo/src/componentes/CampoTexto/index.js
+++ Alura/estudos/javascript/react/organo/src/componentes/CampoTexto/index.js
@@ -4,12 +4,19 @@ const CampoTexto = (props) => {
 
     const placeholderModificado = `${props.placeholder}...`
 
+    let valor = 'Rafael'
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor);
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input required={props.obrigatorio} placeholder={placeholderModificado} />
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificado} />
         </div>
     )
 }
