a9d90d09351ac2f27c32e01871412656f66a34a9
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-19 20:34:45 -0300

    mapeando os colaboradores v5.a4.react:desenvolvendo-com-javascript/react-javascript

--- Alura/estudos/javascript/react/organo/src/componentes/Colaborador/index.js
+++ Alura/estudos/javascript/react/organo/src/componentes/Colaborador/index.js
@@ -1,14 +1,14 @@
 import './Colaborador.css'
 
- const Colaborador = () => {
+ const Colaborador = ({ nome, imagem, cargo }) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src='http://github.com/viniciosneves.png' alt='Vinicios Neves' />
+                <img src={imagem} alt={nome} />
             </div>
             <div className='rodape'>
-                <h4>Vinicios Neves</h4>
-                <h5>Instrutor</h5>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
         </div>
     )
