aaa77190da2aaa34a41b5ec60254c4bcc15de474
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-22 03:07:30 -0300

    inicio do projeto portifolio

--- Alura/testes/teste-react/teste001/wdc/src/App.js
+++ Alura/testes/teste-react/teste001/wdc/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [nome, setNome] = useState('Rafa')
+  function alterarNome() {
+    setNome('Rafael Matheus de Freitas')
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{nome}</h1>
+        <button onClick={alterarNome}>Mudar nome para nome completo</button>
       </header>
     </div>
   );
