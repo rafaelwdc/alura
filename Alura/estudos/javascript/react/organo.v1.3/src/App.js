1048ba95289b66cd94b0cc106acdd55b2099d6cf
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-21 21:00:23 -0300

    criando o botão deletar v3.a2.react:componentes/react-javascript

--- Alura/estudos/javascript/react/organo.v1.3/src/App.js
+++ Alura/estudos/javascript/react/organo.v1.3/src/App.js
@@ -193,14 +193,28 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState(inicial)
 
+  function deletarColaborador() {
+    console.log('deletando colaborador');
+  }
 
   return (
     <div>
       <Banner />
-      <Formulario times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
+      <Formulario 
+        times={times.map(time => time.nome)} 
+        aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} 
+      />
+
       <section className="times">
         <h1>Minha organização</h1>
-        {times.map((time, indice) => <Time key={indice} time={time} colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} />)}
+        {times.map((time, indice) => 
+          <Time 
+            key={indice} 
+            time={time} 
+            colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+            aoDeletar={deletarColaborador}
+          />
+        )}
       </section>
       <Rodape />
     </div>
