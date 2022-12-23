2cd7522028d76f7848725dbbcb61606a574d4f19
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-23 00:26:33 -0300

    Ajustando o index v2.a3.react:estaticos/react-javascript

--- Alura/estudos/javascript/react/alura-space/src/index.js
+++ Alura/estudos/javascript/react/alura-space/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./assets/fontes/GandhiSans-Regular.otf";
+import "./assets/fontes/GandhiSans-Bold.otf";
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
