41faad1f43ac7de77b914fa56b95a9493b897cbd
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-23 19:52:32 -0300

    importação dinamica v3.a4.react:estaticos/react-javascript

---
+++ Alura/estudos/javascript/react/alura-space/src/componentes/Menu/Icone/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Icone({ icone, style }) {
+  return (
+    <li className={style.menu__item}>
+        <img src={icone.path} alt={icone.alt} />
+        <a href="/">{icone.link}</a>
+    </li>
+  )
+}
--- Alura/estudos/javascript/react/alura-space/src/componentes/Menu/index.jsx
+++ Alura/estudos/javascript/react/alura-space/src/componentes/Menu/index.jsx
@@ -1,35 +1,19 @@
 import React from 'react'
-import home from './../../assets/icones/home-ativo.png'
-import maisCurtidas from './../../assets/icones/mais-curtidas-inativo.png'
-import maisVistas from './../../assets/icones/mais-vistas-inativo.png'
-import novas from './../../assets/icones/novas-inativo.png'
-import surpreendaMe from './../../assets/icones/surpreenda-me-inativo.png'
-import styles from './Menu.module.scss'
+import style from './Menu.module.scss'
+import icones from './icones.json'
+import Icone from './Icone'
 
 export default function Menu() {
   return (
-    <nav className={styles.menu}>
-        <ul className={styles.menu__lista}>
-            <li className={styles.menu__item}>
-                <img src={home} alt="ícone do início" />
-                <a href="/">Início</a>
-            </li>
-            <li className={styles.menu__item}>
-                <img src={maisCurtidas} alt="" />
-                <a href="/">Mais Curtidas</a>
-            </li>
-            <li className={styles.menu__item}>
-                <img src={maisVistas} alt="" />
-                <a href="/">Mais Vistas</a>
-            </li>
-            <li className={styles.menu__item}>
-                <img src={novas} alt="" />
-                <a href="/">Novas</a>
-            </li>
-            <li className={styles.menu__item}>
-                <img src={surpreendaMe} alt="" />
-                <a href="/">Surpreenda-me</a>
-            </li>
+    <nav className={style.menu}>
+        <ul className={style.menu__lista}>
+            {icones.map(icone => (
+                <Icone 
+                    key={icone.id} 
+                    icone={icone} 
+                    style={style} 
+                /> 
+            ))}
         </ul>
     </nav>
   )
