99bafff913f3f6c9b4219013e889c835cb8a4edb
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-22 23:27:42 -0300

    criando o menu v1.a2.react:estaticos/react-javascript

--- Alura/estudos/javascript/react/alura-space/src/paginas/PaginaInicial/index.jsx
+++ Alura/estudos/javascript/react/alura-space/src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,11 @@
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
 
 export default function PaginaInicial () {
     return (
-        <Cabecalho />
+        <>
+            <Cabecalho />
+            <Menu />
+        </>
     )
 }
