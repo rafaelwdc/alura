d7fc6b50bbf10c7b38115bc563964384755d82da
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-22 23:53:09 -0300

    transformando o Banner em componente desafio-a2.react:estaticos/react-javascript

--- Alura/estudos/javascript/react/alura-space/src/paginas/PaginaInicial/index.jsx
+++ Alura/estudos/javascript/react/alura-space/src/paginas/PaginaInicial/index.jsx
@@ -1,6 +1,6 @@
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
-import banner from './banner.png';
+import Banner from "../../componentes/Banner"
 import styles from './PaginaInicial.module.scss';
 
 export default function PaginaInicial () {
@@ -8,13 +8,10 @@ export default function PaginaInicial () {
         <>
             <Cabecalho />
             <main>
-                <section className={styles.principal}>
+                <div className={styles.principal}>
                     <Menu />
-                    <div className={styles.principal__imagem}>
-                        <h1>A galeria mais completa do espaço</h1>
-                        <img src={banner} alt="A imagem da terra vista do espaço" />
-                    </div>
-                </section>
+                    <Banner />
+                </div>
             </main>
         </>
     )
