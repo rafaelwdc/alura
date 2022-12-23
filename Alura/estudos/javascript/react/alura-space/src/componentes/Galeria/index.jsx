351fe34a26575b04962c78b494823efdde7476b6
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-23 10:59:41 -0300

    pausa pra descansar v2.a4.react:estaticos/react-javascript

--- Alura/estudos/javascript/react/alura-space/src/componentes/Galeria/index.jsx
+++ Alura/estudos/javascript/react/alura-space/src/componentes/Galeria/index.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import Tags from '../Tags'
+import React from 'react';
+import Tags from '../Tags';
+import styles from './Galeria.module.scss';
+import fotos from './fotos.json'
+import open from './open.png'
+import favorito from './favorito.png'
 
 export default function Galeria() {
   return (
-    <section>
-        <h2>Navegue pela galeria</h2>
-        <Tags />
-        <ul></ul>
+    <section className={styles.galeria}>
+      <h2>Navegue pela galeria</h2>
+      <Tags />
+      <ul className={styles.galeria__cards}>
+        {fotos.map((foto) => {
+          return (
+            <li key={foto.id} className={styles.galeria__card}>
+              <img
+                className={styles.galeria__imagem}
+                src={foto.imagem}
+                alt={foto.titulo}
+              />
+              <p className={styles.galeria__descricao}>{foto.titulo}</p>
+              <div>
+                <p>{foto.creditos}</p>
+                <span>
+                  <img src={favorito} alt="ícone coração de curtir" />
+                  <img src={open} alt="ícone de abrir modal" />
+                </span>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </section>
   )
 }
