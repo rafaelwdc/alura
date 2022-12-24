172e8d1353bb8f0ed0c9647416fdaa0873216fb4
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-24 00:15:23 -0300

    componente Links do react-router-dom v1.a2.react:router/react-javascript

--- Alura/estudos/javascript/react/ola-mundo/ola-mundo/src/App.js
+++ Alura/estudos/javascript/react/ola-mundo/ola-mundo/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Menu from "./componentes/Menu";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 
 function App() {
   return (
     <BrowserRouter>
+      <Menu />
+
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/sobremim" element={<SobreMim />} />
