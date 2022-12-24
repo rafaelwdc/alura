fb81d6f0c332dfb56db06df1414c89917adb9c8e
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-23 23:57:42 -0300

    criando o roteador v5.a1.react:router/react-javascript

--- Alura/estudos/javascript/react/ola-mundo/ola-mundo/src/App.js
+++ Alura/estudos/javascript/react/ola-mundo/ola-mundo/src/App.js
@@ -1,11 +1,15 @@
-import { BrowserRouter } from "react-router-dom"
-import Inicio from "./paginas/inicio";
-import SobreMim from "./paginas/sobreMim";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
 
 function App() {
   return (
     <BrowserRouter>
-      
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
     </BrowserRouter>
   )
 }
