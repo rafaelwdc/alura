0b0cabc6333056ef1773ee1cbfdacbafc3897656
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-26 03:28:49 -0300

    criando rotas v3.a1.react:ContextAPI/react-javascript

--- Alura/estudos/javascript/react/orgs/src/pages/Login/index.js
+++ Alura/estudos/javascript/react/orgs/src/pages/Login/index.js
@@ -7,10 +7,12 @@ import {
 import {
   Input,
   InputLabel,
-  InputAdornment 
+  InputAdornment
 } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
-function Login() {
+function Login({ nome, setNome, saldo, setSaldo }) {
+  const history = useHistory();
   return (
     <Container>
       <Titulo>
@@ -21,6 +23,8 @@ function Login() {
           Nome
         </InputLabel>
         <Input
+          value={nome}
+          onChange={(event) => setNome(event.target.value)}
           type="text"
         />
       </InputContainer>
@@ -29,17 +33,20 @@ function Login() {
           Saldo
         </InputLabel>
         <Input
-        type="number"
-        startAdornment={
-          <InputAdornment position="start">
-            R$
-          </InputAdornment>
-        }
-      />
+          value={saldo}
+          onChange={(event) => setSaldo(event.target.value)}
+          type="number"
+          startAdornment={
+            <InputAdornment position="start">
+              R$
+            </InputAdornment>
+          }
+        />
       </InputContainer>
       <Button
         variant="contained"
         color="primary"
+        onClick={()=> history.push('/feira')}
       >
         Avançar
       </Button>
