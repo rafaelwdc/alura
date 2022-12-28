0b4f549b54a890e234e0d3ec590929d0df3bc560
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-28 05:20:39 -0300

    Criando componente Rodape desafio,.a1.react:praticando/react-javascript

---
+++ Alura/estudos/javascript/react/cinetag/cinetag/src/componentes/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css';
+
+import React from 'react'
+
+export default function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+        <h2>Desenvolvido pela Alura</h2>
+    </footer>
+  )
+}
--- Alura/estudos/javascript/react/cinetag/cinetag/src/pages/Inicio/index.js
+++ Alura/estudos/javascript/react/cinetag/cinetag/src/pages/Inicio/index.js
@@ -1,8 +1,12 @@
 import Cabecalho from 'componentes/Cabecalho'
+import Rodape from 'componentes/Rodape'
 import React from 'react'
 
 export default function Inicio() {
   return (
-    <Cabecalho />
+    <>
+        <Cabecalho />
+        <Rodape />
+    </>
   )
 }
