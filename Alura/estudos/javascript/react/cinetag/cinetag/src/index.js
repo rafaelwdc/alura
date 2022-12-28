7ca6656662dd3e66bd09069d63fbd471e4e64be5
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-28 03:23:54 -0300

    Navegando entre páginas v4.a1.react:praticando/react-javascript

--- Alura/estudos/javascript/react/cinetag/cinetag/src/index.js
+++ Alura/estudos/javascript/react/cinetag/cinetag/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AppRoutes from "routes";
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <h1>Olá Mundo</h1>
+        <AppRoutes />
     </React.StrictMode>
 );
