52f3cce38e56760834dad6b627970fbe232abb1d
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-29 20:15:15 -0300

    adicionando o Titulo v2.a2.react:praticando/react-javascript

--- Alura/estudos/javascript/react/cinetag/cinetag/src/pages/Inicio/index.js
+++ Alura/estudos/javascript/react/cinetag/cinetag/src/pages/Inicio/index.js
@@ -1,5 +1,6 @@
 import Banner from 'componentes/Banner'
 import Cabecalho from 'componentes/Cabecalho'
+import Card from 'componentes/Card'
 import Rodape from 'componentes/Rodape'
 import Titulo from 'componentes/Titulo'
 import React from 'react'
@@ -12,6 +13,7 @@ export default function Inicio() {
         <Titulo>
           <h1>Um lugar para guardar seus vídeos e filmes!</h1>
         </Titulo>
+        <Card id='1' titulo='Gato Bonifáfio' capa='https://thecatapi.com/api/images/get?format=src&type=png' />
         <Rodape />
     </>
   )
