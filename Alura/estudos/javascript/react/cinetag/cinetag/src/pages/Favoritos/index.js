0cfa9e8dbefa004ab2e68d25f89d3d46e9c6acf8
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-29 23:53:34 -0300

    ajustando o css da página Favoritos desafio.a3.react:praticando/react-javascript

--- Alura/estudos/javascript/react/cinetag/cinetag/src/pages/Favoritos/index.js
+++ Alura/estudos/javascript/react/cinetag/cinetag/src/pages/Favoritos/index.js
@@ -1,17 +1,20 @@
 import styles from './Favoritos.module.css'
-
 import React from 'react'
 import Banner from 'componentes/Banner'
 import Titulo from 'componentes/Titulo'
 import Card from 'componentes/Card'
+import { useFavoritoContext } from 'contextos/Favoritos'
 
 export default function Favoritos() {
+  const { favorito } = useFavoritoContext();
   return (
     <>
       <Banner imagem="favoritos" />
       <Titulo> <h1>Meus Favoritos</h1> </Titulo>
       <section className={`${styles.container}`}>
-        <Card id='2' titulo='Foto do Rafael Matheus' capa='https://github.com/rafaelwdc.png' />
+        {favorito.map((fav) => {
+          return <Card {...fav} key={fav.id} />
+        })}
       </section>
     </>
 
