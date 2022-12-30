c82030f473fa4452608805f40643f416f33bd82d
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-29 21:01:04 -0300

    criando card nos favoritos desafio.a2.react:praticando/react-javascript

--- Alura/estudos/javascript/react/cinetag/cinetag/src/pages/Favoritos/index.js
+++ Alura/estudos/javascript/react/cinetag/cinetag/src/pages/Favoritos/index.js
@@ -1,10 +1,19 @@
 import styles from './Favoritos.module.css'
 
 import React from 'react'
+import Banner from 'componentes/Banner'
+import Titulo from 'componentes/Titulo'
+import Card from 'componentes/Card'
 
 export default function Favoritos() {
   return (
     <>
+      <Banner imagem="favoritos" />
+      <Titulo> <h1>Meus Favoritos</h1> </Titulo>
+      <section className={`${styles.container}`}>
+        <Card id='2' titulo='Foto do Rafael Matheus' capa='https://github.com/rafaelwdc.png' />
+      </section>
     </>
+
   )
 }
