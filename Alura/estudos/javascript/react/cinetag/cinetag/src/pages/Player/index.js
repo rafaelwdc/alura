312a3cc5b5cbefa8e37530e0616ce0ba1fb13932
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-30 00:59:45 -0300

    criando rota dinamica para os videos v1.a4.react:praticando/react-javascript

---
+++ Alura/estudos/javascript/react/cinetag/cinetag/src/pages/Player/index.js
@@ -0,0 +1,38 @@
+import styles from './Player.module.css'
+
+import React from 'react'
+import Banner from 'componentes/Banner'
+import Titulo from 'componentes/Titulo'
+import videos from 'json/db.json'
+import { useParams } from 'react-router-dom'
+
+export default function Player() {
+    const parametros = useParams();
+    const video = videos.find((video) => {
+        return video.id === Number(parametros.id);
+    });
+    return (
+        <>
+            <Banner imagem="player" />
+            <Titulo>
+                <h1>Player</h1>
+            </Titulo>
+            <section className={styles.container}>
+                <iframe
+                    width="100%"
+                    height="100%"
+                    src={video.link}
+                    title={video.titulo}
+                    frameborder="0"
+                    allow="accelerometer; 
+                        autoplay; 
+                        clipboard-write; 
+                        encrypted-media; 
+                        gyroscope; 
+                        picture-in-picture"
+                    allowfullscreen
+                ></iframe>
+            </section>
+        </>
+    )
+}
--- Alura/estudos/javascript/react/cinetag/cinetag/src/routes.js
+++ Alura/estudos/javascript/react/cinetag/cinetag/src/routes.js
@@ -4,6 +4,7 @@ import Rodape from "componentes/Rodape";
 import FavoritosProvider from "contextos/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
+import Player from "pages/Player";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function AppRoutes() {
@@ -15,6 +16,7 @@ export default function AppRoutes() {
                     <Routes>
                         <Route path="/" element={<Inicio />}></Route>
                         <Route path="/favoritos" element={<Favoritos />}></Route>
+                        <Route path="/:id" element={<Player />}></Route>
                     </Routes>
                 </FavoritosProvider>
             </Container>
