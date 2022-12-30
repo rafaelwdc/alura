b64be117f0b2e42c92ef9e81e89525f0e2fd31cd
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-30 01:34:34 -0300

    adicionando o hook useEffect v2.a5.react:praticando/react-javascript

--- Alura/estudos/javascript/react/cinetag/cinetag/src/pages/Inicio/index.js
+++ Alura/estudos/javascript/react/cinetag/cinetag/src/pages/Inicio/index.js
@@ -1,22 +1,30 @@
 import Banner from 'componentes/Banner'
 import Card from 'componentes/Card'
 import Titulo from 'componentes/Titulo'
-import videos from 'json/db.json'
 import styles from './Inicio.module.css'
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Inicio() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/rafaelwdc/cinetag-api/videos')
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setVideos(dados);
+      })
+  }, [])
   return (
     <>
-        <Banner imagem="home"/>
-        <Titulo>
-          <h1>Um lugar para guardar seus vídeos e filmes!</h1>
-        </Titulo>
-        <section className={styles.container}>
-          {videos.map((video) => {
-            return <Card {...video} key={video.id} />
-          })}
-        </section>
+      <Banner imagem="home" />
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />
+        })}
+      </section>
     </>
   )
 }
