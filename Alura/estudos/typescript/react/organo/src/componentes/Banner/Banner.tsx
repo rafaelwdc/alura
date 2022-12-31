b21abf68a3d40ff5989d884f6bd32457f494f5a3
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2022-12-31 19:52:32 -0300

    gerando tsconfig e primeira migração v5.a1.react:migrando-typescript/react-javascript

---
+++ Alura/estudos/typescript/react/organo/src/componentes/Banner/Banner.tsx
@@ -0,0 +1,20 @@
+import './Banner.css'
+
+import React from 'react'
+
+interface BannerProps {
+    enderecoImagem: string
+    textoAlternativo?: string
+}
+
+export const Banner = ({ enderecoImagem, textoAlternativo } :BannerProps) => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src={enderecoImagem} alt={textoAlternativo}/>
+            {/* <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/> */}
+        </header>
+    )
+}
+
+export default Banner
