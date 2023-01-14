1b76a1ea948016072dff28c3e2efafaba699e7d8
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-14 20:53:42 -0300

    atualizando o componente Botao

--- Alura/estudos/typescript/react/alura-studies/src/components/Botao/index.tsx
+++ Alura/estudos/typescript/react/alura-studies/src/components/Botao/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 class Botao extends React.Component {
-    render() {
-        return(
-            <button>
+    render () {
+        const estaAtivo = true
+        const styles = {
+            backgroundColor: estaAtivo ? 'green' : 'red'
+        }
+        return (
+            <button style={styles}>
                 Botão
             </button>
-        )
-    }
-}
+        );
+    };
+};
 
 export default Botao;
