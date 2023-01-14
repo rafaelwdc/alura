0601fb94a7ec3574cebcb54221c04d04202bf664
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-14 19:51:37 -0300

    atualizando o index.html

--- Alura/estudos/typescript/react/alura-studies/src/App.tsx
+++ Alura/estudos/typescript/react/alura-studies/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Botao from './components/Botao';
 
 function App() {
   return (
     <div className="App">
-      Olá, Mundo!
+      <Botao />
     </div>
   );
 }
