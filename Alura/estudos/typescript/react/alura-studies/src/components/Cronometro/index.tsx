59920ee8d3a1397fd2e71cdd0b31c7c694e277db
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-14 23:15:16 -0300

    adicionando novos componentes

---
+++ Alura/estudos/typescript/react/alura-studies/src/components/Cronometro/index.tsx
@@ -0,0 +1,19 @@
+import Botao from "../Botao";
+import Relogio from "./Relogio";
+import style from './Cronometro.module.scss'
+
+export function Cronometro () {
+    return (
+        <div className={style.cronometro}>
+            <p className={style.titulo}>
+                Escolha um card e inicie o Cronometro
+            </p>
+            <div className={style.relogioWrapper}>
+                <Relogio />
+            </div>
+            <Botao>
+                Começar!
+            </Botao>
+        </div>
+    )
+}
