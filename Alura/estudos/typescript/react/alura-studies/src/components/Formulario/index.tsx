1d25ed2cfad0821d4ad9b9a065de512a4137512e
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-16 23:09:06 -0300

    atualizando o componente Item

--- Alura/estudos/typescript/react/alura-studies/src/components/Formulario/index.tsx
+++ Alura/estudos/typescript/react/alura-studies/src/components/Formulario/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { iTarefa } from "../../types/tarefas";
 import Botao from "../Botao";
 import style from './Formulario.module.scss'
+import { v4 as uuidv4 } from 'uuid'
 
 class Formulario extends React.Component<{
     setTarefas: React.Dispatch<React.SetStateAction<iTarefa[]>>
@@ -12,7 +13,21 @@ class Formulario extends React.Component<{
     }
     adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
         evento.preventDefault();
-        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, { ...this.state }])
+        this.props.setTarefas(tarefasAntigas => 
+            [
+                ...tarefasAntigas, 
+                { 
+                    ...this.state,
+                    selecionado: false,
+                    completado: false,
+                    id: uuidv4() 
+                }
+            ]
+        )
+        this.setState({
+            tarefa: "",
+            tempo: "00:00"
+        })
     }
     render() {
         return (
