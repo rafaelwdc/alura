53b54e4612694ac14246762e40103d274f0f60d8
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-17 00:25:35 -0300

    atualizando o componente Item

--- Alura/estudos/typescript/react/alura-studies/src/components/Lista/Item/index.tsx
+++ Alura/estudos/typescript/react/alura-studies/src/components/Lista/Item/index.tsx
@@ -1,11 +1,34 @@
 import { iTarefa } from '../../../types/tarefas'
 import style from '../Lista.module.scss'
 
-export default function Item({ tarefa, tempo, selecionado, completado, id }: iTarefa) {
+interface Props extends iTarefa {
+    selecionaTarefa: (tarefaSelecionada: iTarefa) => void
+}
+
+export default function Item(
+    { 
+        tarefa, 
+        tempo, 
+        selecionado, 
+        completado, 
+        id, 
+        selecionaTarefa 
+    }: Props) {
     console.log('Item atual: ', { tarefa, tempo, selecionado, completado, id });
     
     return (
-        <li className={style.item}>
+        <li 
+            className={style.item} 
+            onClick={() => selecionaTarefa(
+                {
+                    tarefa, 
+                    tempo,
+                    selecionado,
+                    completado,
+                    id
+                }
+            )}
+        >
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
         </li>
