cdcc93ad5a6a7ec918c433dbbee2f3becc09246f
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-16 23:09:29 -0300

    atualizando o update do componente Item

--- Alura/estudos/typescript/react/alura-studies/src/components/Lista/Item/index.tsx
+++ Alura/estudos/typescript/react/alura-studies/src/components/Lista/Item/index.tsx
@@ -1,6 +1,9 @@
+import { iTarefa } from '../../../types/tarefas'
 import style from '../Lista.module.scss'
 
-export default function Item({ tarefa, tempo }: {tarefa: string, tempo: string}) {
+export default function Item({ tarefa, tempo, selecionado, completado, id }: iTarefa) {
+    console.log('Item atual: ', { tarefa, tempo, selecionado, completado, id });
+    
     return (
         <li className={style.item}>
             <h3>{tarefa}</h3>
