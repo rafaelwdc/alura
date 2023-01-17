b8db1f0abaa9aa3d5b7781ce285f8dfbdc2de4d8
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-16 21:02:05 -0300

    atualizando o componente Formulario

--- Alura/estudos/typescript/react/alura-studies/src/components/Formulario/index.tsx
+++ Alura/estudos/typescript/react/alura-studies/src/components/Formulario/index.tsx
@@ -3,9 +3,18 @@ import Botao from "../Botao";
 import style from './Formulario.module.scss'
 
 class Formulario extends React.Component {
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    }
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        console.log('state: ', this.state);
+        
+    }
     render() {
         return (
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo    
@@ -13,6 +22,8 @@ class Formulario extends React.Component {
                     <input 
                         type="text"
                         name="tarefa"
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
                         id="tarefa"
                         placeholder="O que você quer estudar?" 
                         required
@@ -26,6 +37,8 @@ class Formulario extends React.Component {
                         type="time"
                         step="1"
                         name="tempo"
+                        value={this.state.tempo}
+                        onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
                         id="tempo"
                         min="00:00:00"
                         max="01:30:00"
