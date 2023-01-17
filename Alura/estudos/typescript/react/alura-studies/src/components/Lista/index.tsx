f89a2f390b574248f6810021fbe500c7ff8ae783
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-16 22:37:38 -0300

    atualizando o componente Lista

--- Alura/estudos/typescript/react/alura-studies/src/components/Lista/index.tsx
+++ Alura/estudos/typescript/react/alura-studies/src/components/Lista/index.tsx
@@ -1,23 +1,12 @@
-import React, { useState } from 'react';
+import { iTarefa } from '../../types/tarefas';
 import Item from './Item';
 import style from './Lista.module.scss'
 
-function Lista () {
-    const [tarefas, setTarefas] = useState([{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    },{
-        tarefa: 'JavaScript',
-        tempo: '01:00:00'
-    },{
-        tarefa: 'TypeScript',
-        tempo: '03:00:00'
-    }])
+function Lista ({ tarefas }: {tarefas: iTarefa[]}) {
+    
     return (
         <aside className={style.listaTarefas}>
-            <h2 onClick={() => {                
-                setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00"}])
-            }}>Estudos do Dia</h2>
+            <h2>Estudos do Dia</h2>
             <ul>
                 {tarefas.map((item, index) =>  (
                     <Item
