1781a3cada8baa19f619d0e1cfb5ab770db0cb66
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-17 22:36:10 -0300

    atualizando o componente Cronometro

--- Alura/estudos/typescript/react/alura-studies/src/components/Cronometro/index.tsx
+++ Alura/estudos/typescript/react/alura-studies/src/components/Cronometro/index.tsx
@@ -1,13 +1,27 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss'
+import { iTarefa } from "../../types/tarefas";
+import { useEffect, useState } from "react"
+import { tempoParaSegundos } from "../../common/utils/time";
 
-export function Cronometro () {
+interface Props {
+    selecionado: iTarefa | undefined
+}
+
+export function Cronometro ({ selecionado }: Props) {
+    const [tempo, setTempo] = useState<number>();
+    useEffect(() => {
+        if(selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    }, [selecionado])
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>
                 Escolha um card e inicie o Cronometro
             </p>
+            Tempo: {tempo}
             <div className={style.relogioWrapper}>
                 <Relogio />
             </div>
