13fca9bdd27000546cc83a1b8ba1cb1ec928d1ad
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-03-09 19:36:01 -0300

    atualização de projetos

---
+++ Alura/estudos/Alura VSC/app.js
@@ -0,0 +1,47 @@
+// idades = [32, 30, 28]
+// nomes = ["Patricky", "Rafael", "Camila"]
+// faculdade = [false, false, true]
+
+// funcionarios = [nomes, idades, faculdades]
+
+// function eMaiorQue10(value) {
+//      return value >= 10;
+// }
+
+// var filtrado = numeros.filter(eMaiorQue10);
+// // filtrado é [12, 130, 44]
+
+// console.log(filtrado);
+
+const notas1 = [10, 6.5, 8, 7.5]
+const notas2 = [9, 6, 6]
+const notas3 = [8.5, 9.5]
+
+const notasGerais = [notas1, notas2, notas3]
+
+let media = 0
+
+for (let i = 0; i < notasGerais.length; i++) {
+    for (let j = 0; j < notasGerais[i].length; j++) {
+        media += notasGerais[i][j] / notasGerais[i].length;
+    }
+}
+
+media = media / notasGerais.length;
+
+console.log(media);
+
+teste()
+
+function teste() {
+    console.log("retornou")
+    return 10
+}
+
+let TextoDeQuebraLinha = "Oi, tudo bem? Eu sou o Rafael, esse texto é longo ....lorem ipslun"
+
+array = []
+array.push(1)
+console.log(array);
+
+let texto = ["oi", "ok"]
--- Alura/estudos/html_css/acessibilidade/acessibilidade001/js/validacao-cep.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('#cep').oninvalid = 
-function() {
-    this.setCustomValidity('');
-    if(!this.validity.valid) {
-        this.setCustomValidity('Ops! Tem algo de errado nesse campo');
-        this.parentNode.classList.add('contatoCampo--erro')
-    };
-};
