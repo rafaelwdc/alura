301b815700f8b8ed1924f905f13b956efaaa64b7
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-03-09 20:46:04 -0300

    app.js att

--- Alura/estudos/Alura VSC/app.js
+++ Alura/estudos/Alura VSC/app.js
@@ -34,7 +34,6 @@ console.log(media);
 teste()
 
 function teste() {
-    console.log("retornou")
     return 10
 }
 
@@ -44,4 +43,9 @@ array = []
 array.push(1)
 console.log(array);
 
-let texto = ["oi", "ok"]
+let PalavrasSimples = ["oi", "ok"]
+
+for (let i = 0; i < PalavrasSimples.length; i++) {
+    const element = PalavrasSimples[i];
+    
+}
